921c79205010877a765a94bf3189b28e81c916b8
Erico <erico@appnovation.com>
2013-08-21 15:45:29 -0700

    reports dropdown

--- docroot/sites/all/modules/custom/sbac_report/templates/sbac-report-date-range-dropdown.tpl.php
+++ docroot/sites/all/modules/custom/sbac_report/templates/sbac-report-date-range-dropdown.tpl.php
@@ -10,9 +10,11 @@
   <span class="form-required" title="This field is required.">*</span>
   </label>
   <a href="#" id="date-selected" class="report-dropdown-toggle"><?php print $default_display; ?></a>
-  <ul id="date-range-dropdown" class="report-dropdown hide">
-    <li><a class="date-range-item report-dropdown-toggle" data-value="<?php print $ago_30; ?>"><?php print t('Last 30 days'); ?></a></li>
-    <li><a class="date-range-item report-dropdown-toggle" data-value="<?php print $ago_60; ?>"><?php print t('Last 60 days'); ?></a></li>
-    <li><a class="date-range-item report-dropdown-toggle" data-value="<?php print $ago_90; ?>"><?php print t('Last 90 days'); ?></a></li>
-    <li><?php print t('Custom Date'); ?></li>
+  <div id="date-range-dropdown" class="report-dropdown hide">
+    <ul>
+      <li><a class="date-range-item report-dropdown-toggle" data-value="<?php print $ago_30; ?>"><?php print t('Last 30 days'); ?></a></li>
+      <li><a class="date-range-item report-dropdown-toggle" data-value="<?php print $ago_60; ?>"><?php print t('Last 60 days'); ?></a></li>
+      <li><a class="date-range-item report-dropdown-toggle" data-value="<?php print $ago_90; ?>"><?php print t('Last 90 days'); ?></a></li>
+      <li><?php print t('Custom Date'); ?></li>
+    </ul>
 <?php // The closing tags are attached to the date popup form element. ?>
