661aa1f36fd37956a1176c814e88b72b7a2728d6
teknic <btate340@gmail.com>
2013-08-25 16:06:02 -0700

    fixed

--- docroot/sites/all/modules/custom/sbac_report/js/sbac_report.individual.js
+++ docroot/sites/all/modules/custom/sbac_report/js/sbac_report.individual.js
@@ -13,6 +13,16 @@
       $uidField.val(uid);
     });
 
+    $('#autocomplete li').keypress( function(event) {
+      var keypressed = event.which;
+      if(keypressed == 13){
+//        var uid = $(this).find('.autocomplete-suggestion').data('uid');
+//        $uidField.text(uid);
+//        $uidField.val(uid);
+        $(this).click();
+      }
+    });
+
     // Individual Report: clear the id on event "input".
     $('#edit-sne').bind('input', function(e) {
       if ($uidField.val() != '') {
