404c668e8de38141d548621401b7765c0e7680a8
Yuriy Babenko <yuriyb@gmail.com>
2013-08-26 12:05:03 -0700

    fixed PHP notices

--- docroot/sites/all/themes/SBAC/templates/user-profile.tpl.php
+++ docroot/sites/all/themes/SBAC/templates/user-profile.tpl.php
@@ -62,9 +62,9 @@
 				//$school = drupal_render($user_profile['field_school_name']);
 				//$district = drupal_render($user_profile['field_district_name']);
 				//$state = drupal_render($user_profile['field_state']);
-        $school = $user_profile['field_school_name']['#items'][0]['value'];
-        $district = $user_profile['field_district_name']['#items'][0]['value'];
-        $state = $user_profile['field_state'][0]['#title'];
+        $school = isset($user_profile['field_school_name']['#items'][0]['value']) ? $user_profile['field_school_name']['#items'][0]['value'] : NULL;
+        $district = isset($user_profile['field_district_name']['#items'][0]['value']) ? $user_profile['field_district_name']['#items'][0]['value'] : NULL;
+        $state = isset($user_profile['field_state'][0]['#title']) ? $user_profile['field_state'][0]['#title'] : NULL;
 
 				if ($school && $district && $state) {
 					echo t('!school in !district, !state', array(
