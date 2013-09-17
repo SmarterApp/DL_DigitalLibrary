340e3e3cc6df5685570120d778b079305825183d
teknic <btate340@gmail.com>
2013-09-17 12:13:16 -0700

    updated

--- docroot/sites/all/modules/custom/sbac_media/js/sbac_media.form.js
+++ docroot/sites/all/modules/custom/sbac_media/js/sbac_media.form.js
@@ -8,11 +8,23 @@
    */
   Drupal.behaviors.sbac_media_form_element = {
     attach: function (context, settings) {
+      // Hide the modal button.
       $('#sbac-media-internet-confirm').hide();
+
+      // Make the table sortable.
       var selector = '#sbac-media-list tbody';
       $(selector).sortable({
         items: 'tr'
       }).disableSelection();
+
+      // On change, take the input and replace the modal HREF.
+      $('#edit-field-embed-video').change( function () {
+        var embed_code = $(this).val();
+        var modal_link = $('#sbac-media-internet-confirm');
+        var href = $(modal_link).attr('href');
+        var new_href = href.replace("SBAC-MEDIA-EMBEDDED", embed_code);
+        modal_link.attr('href', new_href);
+      });
     }
   };
 
