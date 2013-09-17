82d53fa57c83c5e42d299f8778fc3ddaacc1d765
Yuriy Babenko <yuriyb@gmail.com>
2013-09-17 16:18:27 -0700

    review flow

--- docroot/sites/all/modules/custom/sbac_core/components/review/js/review.js
+++ docroot/sites/all/modules/custom/sbac_core/components/review/js/review.js
@@ -4,9 +4,46 @@ if (typeof Review == 'undefined') {
   Review = {};
 }
 
+Review.reviews = {
+  form_trigger: '.review-form-trigger',
+  form: '#eck-entity-form-add-review-end-use',
+
+  utilities: {
+    toggle: function(trigger, target) {
+      var trigger_element = $(trigger);
+      var target_element = $(target);
+
+      if (trigger_element.length && target_element.length) {
+        trigger_element.click(function(e) {
+          e.preventDefault();
+
+          target_e
+
+
+          return false;
+        });
+      }
+    }
+  },
+};
+
+
 Drupal.behaviors.review = {
   attach: function (context, settings) {
-    
+    // form showing functionality on 'Write Review' button
+    var trigger_element = $(Review.reviews.form_trigger);
+    var target_element = $(Review.reviews.form);
+
+    if (trigger_element.length && target_element.length) {
+      trigger_element.click(function(e) {
+        e.preventDefault();
+
+        trigger_element.hide();
+        target_element.show();
+
+        return false;
+      });
+    }
   }
 };
 
