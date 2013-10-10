255d619b2012c7dca95535f4f88918c645fa442b
teknic <btate340@gmail.com>
2013-10-09 22:19:09 -0700

    added alignment js

--- docroot/sites/all/themes/SBAC/js/custom.js
+++ docroot/sites/all/themes/SBAC/js/custom.js
@@ -1,217 +1,23 @@
 (function ($) {
   Drupal.behaviors.sbac_custom = {
     attach: function (context, settings) {
-
-
-      $('.disabled').click(function(e) {
-          e.preventDefault();
-          //do other stuff when a click happens
-      });
-
-      $('.ccss-term-delete').click(function(){
-        var nid = $(this).attr('nid');
-        var update_form = function(data) {
-          $('tr#term-' + nid).hide();
-        }
-
+      $('#disable-feedback').click(function () {
         $.ajax({
-          type: "POST",
-          url: "/ajax-alignment-crud",
-          success: update_form,
-          data:'op=delete&nid=' + nid
-        });
-        return false;
-      });
-
-      $('.sbac-custom-term-remove').click(function () {
-        var parentTerm = $(this);
-        var parentId = $(this).attr("tid");
-        var update_data = function(data) {
-          var obj = jQuery.parseJSON(data);
-          if ((obj.publication == 'SBAC-ELA-v1' && obj.depth <= 3)
-              || (obj.publication == 'SBAC-MA-v1' && obj.depth <= 5)
-              || (obj.publication == '0')){
-            $('.alignment-form').hide();
-            $('.alignment-buttons').show();
-          }
-
-          $('.alignment-filter').html(obj.html);
-          var attr = parentTerm.parent('li').attr('id');
-          $("#" + attr + " ~ li").remove();
-          parentTerm.parent('li').remove();
-
-          //reattached the behaviors
-          Drupal.attachBehaviors('.sbac-custom-term-remove');
-          $('.disabled').click(function(e) {
-              e.preventDefault();
-              //do other stuff when a click happens
-          });
-        }
-
-        $.ajax({
-          type: "POST",
-          url: "/ajax-terms",
-          success: update_data,
-          data:'parent=' + parentId + '&remove=true',
-        });
-
-        return false;
-      });
-
-      $('.sbac-custom-term').click( function () {
-        var parentId = $(this).attr("tid");
-
-        var update_data = function(data) {
-          var obj = jQuery.parseJSON(data);
-
-          if ((obj.publication == 'SBAC-ELA-v1' && obj.depth > 3)
-              || (obj.publication == 'SBAC-MA-v1' && obj.depth > 5)){
-            $('.alignment-form').show();
-            $('.alignment-buttons').hide();
-            $('.alignment-filter').html('');
-
-            var update_form = function(data) {
-              var obj = jQuery.parseJSON(data);
-              $('.alignment-form').html(obj.html);
-              $('p[id^=description-]').more({length:200, moreText: 'read more', lessText: 'read less'});
-
-              $('#ccss-cancel').click(function() {
-                $('#modalBackdrop').hide();
-                $('#modalContent').hide();
-              });
-
-              $('#ccss-submit').click(function() {
-                var countType = countStandard = 0;
-                $('#alignment-msg').html('');
-                //counts type
-                $('#edit-alignment-type option').each(function() {
-                  if($(this).is(':selected') && $(this).val() != ''){
-                    countType++;
-                  }
-                });
-                //count standards
-                $('input[id^=edit-term-]').each(function() {
-                  if($(this).is(':checked')){
-                    countStandard++;
-                  }
-                });
-
-                if (countStandard > 0 && countType > 0) {
-                  var alignmentStandards = alignmentType = '';
-
-                  //get ref
-                  var alignmentRef = $('input[id=alignment_ref]').val();
-                  //counts type
-                  $('#edit-alignment-type option').each(function() {
-                    if($(this).is(':selected') && $(this).val() != ''){
-                      alignmentType = $(this).val();
-                    }
-                  });
-                  //count standards
-                  $('input[id^=edit-term-]').each(function() {
-                    if($(this).is(':checked')){
-                      var temp = $(this).attr('id');
-                      var id = temp.split('-');
-                      id = id[2];
-                      alignmentStandards += '|' + id;
-                    }
-                  });
-
-                  var closeModal = function(data) {
-                    var obj = jQuery.parseJSON(data);
-                    $('#sbac-resource-alignment-tag-view').html(obj.html);
-                    $('#modalBackdrop').hide();
-                    $('#modalContent').hide();
-                    Drupal.attachBehaviors('.ccss-term-delete');
-                  }
-
-                  $.ajax({
-                    type: "POST",
-                    url: "/ajax-alignment-crud",
-                    success: closeModal,
-                    data:'op=create&alignment_type=' + alignmentType + '&alignment_ref=' + alignmentRef + '&alignment_standards=' + alignmentStandards,
-                  });
-                }
-                else {
-                  $('#modal-content').animate({ scrollTop: 0 });
-                  $('#alignment-msg').append('<div class="alignment-error"><ul></ul></div>');
-                  if (countStandard < 1) {
-                    $('#alignment-msg .alignment-error ul').append('<li>Please select a standard.</li>');
-                  }
-                  if (countType < 1) {
-                    $('#alignment-msg .alignment-error ul').append('<li>Please select alignment type.</li>');
-                  }
-                }
-              });
-
-
-
-
-            }
-
-            var refNode = $('input#ref_node').val();
-
-            $.ajax({
-              type: "POST",
-              url: "/ajax-alignment-form",
-              success: update_form,
-              data:'tid=' + parentId + '&ref_node=' + refNode
-            });
-          }
-          else {
-            $('.alignment-form').hide();
-            $('.alignment-buttons').show();
-            $('.alignment-filter').html(obj.html);
-
-            $('.disabled').click(function(e) {
-                e.preventDefault();
-                //do other stuff when a click happens
-            });
-          }
-
-          //reattached the behaviors
-          Drupal.attachBehaviors('.sbac-custom-term');
-        }
-
-        $.ajax({
-          type: "POST",
-          url: "/ajax-terms",
-          success: update_data,
-          data:'parent=' + parentId ,
-        });
-
-        var update_breadcrumb = function(data) {
-          var obj = jQuery.parseJSON(data);
-          $('.alignment-breadcrumb').html(obj.html);
-
-          Drupal.attachBehaviors('.sbac-custom-term-remove');
-        }
-
-        $.ajax({
-          type: "POST",
-          url: "/ajax-alignment-breadcrumbs",
-          success: update_breadcrumb,
-          data:'tid=' + parentId ,
-        });
-
-
-
-
-        return false;
-      });
-
-       $('#disable-feedback').click(function() {
-        $.ajax({
-           url: Drupal.settings.basePath + 'disable-feedback',
-           success: function (data, textStatus, jqXHR) {
-            if (data == 0) {
-              document.getElementById('feedback-click').click();
-            }
+          url: Drupal.settings.basePath + 'disable-feedback',
+          success: function (data, textStatus, jqXHR) {
+            // if (data == 0) {
+            //   document.getElementById('feedback-click').click();
+            // }
             document.getElementById('feedback-click').click();
-
-           }
+            $('#feedback-dropdown').removeClass('open');
+          }
         });
-       });
+      });
+
+      $('.disabled').click(function (e) {
+        e.preventDefault();
+        //do other stuff when a click happens
+      });
     }
   };
 })(jQuery);
