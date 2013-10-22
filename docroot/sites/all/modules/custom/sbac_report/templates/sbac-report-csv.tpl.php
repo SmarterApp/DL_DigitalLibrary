9be78d9359bff6d30edadd5b045b4cbd3f1f6ccb
Robb Lovell <robblovell@Robbs-MacBook-Air.local>
2013-10-22 14:37:49 -0700

    work in progress, for user activity report.

--- docroot/sites/all/modules/custom/sbac_report/templates/sbac-report-csv.tpl.php
+++ docroot/sites/all/modules/custom/sbac_report/templates/sbac-report-csv.tpl.php
@@ -7,7 +7,4 @@
 <h2><?php print t('CSV Report'); ?></h2>
 <div class="report-form-container"><?php print drupal_render($form); ?></div>
 <hr>
-<?php if (!empty($csv_activity_report)) : ?>
-  <h2><?php print t('Activity Report'); ?></h2>
-  <?php print $csv_activity_report; ?>
-<?php endif ?>
+
