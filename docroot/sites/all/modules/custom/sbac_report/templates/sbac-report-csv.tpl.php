4679dfc55ce4443e99ccbcd86050a18392f9d84e
Erico <erico@appnovation.com>
2013-10-23 14:17:50 -0700

    reports clear and polishing

--- docroot/sites/all/modules/custom/sbac_report/templates/sbac-report-csv.tpl.php
+++ docroot/sites/all/modules/custom/sbac_report/templates/sbac-report-csv.tpl.php
@@ -4,7 +4,6 @@
  * Template for SBAC Individual Report page.
  */
 ?>
-<h2><?php print t('CSV Report'); ?></h2>
+<h2 class="report-title"><?php print t('CSV Report'); ?></h2>
 <div class="report-form-container"><?php print drupal_render($form); ?></div>
-<hr>
 
