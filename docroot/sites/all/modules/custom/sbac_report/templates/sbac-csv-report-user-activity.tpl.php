b26219774b155d1bbe03b5fbd19c69ce626c92db
Robb Lovell <robblovell@Robbs-MacBook-Air.local>
2013-10-25 18:16:58 -0700

    Niether for SNE/SLT filter.  DLRB Member removed from admins, added to user group drop down.  Instruction test for the user activity report.

--- docroot/sites/all/modules/custom/sbac_report/templates/sbac-csv-report-user-activity.tpl.php
+++ docroot/sites/all/modules/custom/sbac_report/templates/sbac-csv-report-user-activity.tpl.php
@@ -5,4 +5,11 @@
  */
 ?>
 <h2 class="report-title"><?php print t('User Activity (CSV Report)'); ?></h2>
+Includes site activity statistics for all users, including:
+<ul>
+  <li>Basic user information</li>
+  <li>Activity in the Digital Library</li>
+  <li>User contributions and reviews</li>
+  <li>Moderation activity</li>
+</ul>
 <div class="report-form-container"><?php print drupal_render($form); ?></div>
