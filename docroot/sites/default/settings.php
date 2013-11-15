0858f426541977c4f395404ea4c20409eb95d238
teknic <btate340@gmail.com>
2013-11-15 08:30:02 -0800

    added file system to settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -670,6 +670,36 @@ if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
 //  $conf['apachesolr_default_environment'] = 'amazon_apache_solr_environment_local';
 //}
 
+// Sets the Apache Solr Environment
+if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
+  switch ($_ENV['AH_SITE_ENVIRONMENT']) {
+    case 'dev':
+      $conf['file_private_path'] = '/mnt/files/sbacdev/files-private';
+      // do something on dev
+      break;
+
+    case 'test':
+      // do something on staging
+      $conf['file_private_path'] = '/mnt/files/sbacstg/files-private';
+      break;
+
+    case 'stage2':
+      // do something on staging
+      $conf['file_private_path'] = '/mnt/files/sbacstg2/files-private';
+      break;
+
+    case 'stage3':
+      // do something on staging
+      $conf['file_private_path'] = '/mnt/files/sbacstg3/files-private';
+      break;
+
+    case 'prod':
+      // do something on prod
+      $conf['file_private_path'] = '/mnt/files/sbac/files-private';
+      break;
+  }
+}
+
 error_reporting(E_ERROR);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
