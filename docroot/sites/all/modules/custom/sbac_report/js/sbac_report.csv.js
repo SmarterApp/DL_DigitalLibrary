297161ea4102615e5c9e0d09b554c2cca55b5f56
teknic <btate340@gmail.com>
2013-11-20 12:23:23 -0800

    fixed

--- docroot/sites/all/modules/custom/sbac_report/js/sbac_report.csv.js
+++ docroot/sites/all/modules/custom/sbac_report/js/sbac_report.csv.js
@@ -1,11 +1,1 @@
-///**
-// * CSV Report.
-// */
-(function($) {
-  // DOM ready.
-    $(function() {
-        if ($("div#id, div.alert-box.secondary, h2#element-invisible").text().indexOf("Warning message") == 1) {
-          setTimeout(function ()  {  $(".alert-box.secondary").hide(); }, 10000);
-        }
-    });
-})(jQuery);
+
