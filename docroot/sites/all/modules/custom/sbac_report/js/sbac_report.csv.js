b2da833bf01eb7f6871514d8481a3f15584ebdb4
teknic <btate340@gmail.com>
2013-11-22 15:39:45 -0800

    fixing

--- docroot/sites/all/modules/custom/sbac_report/js/sbac_report.csv.js
+++ docroot/sites/all/modules/custom/sbac_report/js/sbac_report.csv.js
@@ -1,1 +1,14 @@
+(function ($) {
+  Drupal.behaviors = Drupal.behaviors || {};
 
+  Drupal.behaviors.sbac_report_csv = {
+    attach: function (context, settings) {
+//      setTimeout(function(){ $('.sbac-report-download-link').click(); }, 5000);
+//      not sure why this is not working...
+//      $('.sbac-report-download-link').click(function (e) {
+//        alert('hi');
+//      });
+    }
+  };
+
+})(jQuery);
