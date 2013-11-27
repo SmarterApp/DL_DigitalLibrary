c563c72b956ffe6aeda9654f6b0fd590d7599a70
teknic <btate340@gmail.com>
2013-11-27 11:55:34 -0800

    updated

--- docroot/sites/all/modules/custom/sbac_favorites/templates/no-favorites.tpl.php
+++ docroot/sites/all/modules/custom/sbac_favorites/templates/no-favorites.tpl.php
@@ -1,2 +1,2 @@
 <h2>You have no resource in your favorites</h2>
-<a href="digital-library-resources">Browse Digital Library</a> Resources and add them to the favorites by clicking 'star' icon.
+<a href="digital-library-resources">Browse Digital Library</a> Resources and add them to the favorites by clicking <span class="star-icon">star</span> icon.
