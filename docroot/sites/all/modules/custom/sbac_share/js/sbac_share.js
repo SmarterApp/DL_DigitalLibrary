f6a38cacbdfb204ad2b8b024fbe9f38b08248131
teknic <btate340@gmail.com>
2013-12-03 15:38:50 -0800

    updates

--- docroot/sites/all/modules/custom/sbac_share/js/sbac_share.js
+++ docroot/sites/all/modules/custom/sbac_share/js/sbac_share.js
@@ -3,6 +3,39 @@
 
   Drupal.behaviors.sbac_share = {
     attach: function (context, settings) {
+      var clip = new ZeroClipboard(document.getElementById('sbac-permanent-link-button'), {
+        moviePath: 'http://' + location.hostname + '/sites/all/libraries/zeroclipboard/ZeroClipboard.swf'
+      });
+
+      // clip.glue(document.getElementById('sbac-permanent-link-text'));
+
+      clip.on( 'load', function(client) {
+        alert( "movie is loaded" );
+      } );
+
+      clip.on( 'complete', function(client, args) {
+        alert("Copied text to clipboard: " + args.text );
+      } );
+
+      clip.on( 'mouseover', function(client) {
+        alert("mouse over");
+      } );
+
+      clip.on( 'mouseout', function(client) {
+        alert("mouse out");
+      } );
+
+      clip.on( 'mousedown', function(client) {
+        alert("mouse down");
+        return false;
+      } );
+
+      clip.on( 'mouseup', function(client) {
+        alert("mouse up");
+        return false;
+      } );
+
+
       $('#sbac-temp-link-button').click( function() {
         var nid = $(this).attr('nid');
         generate_link(nid);
@@ -30,36 +63,6 @@
           }
         });
       };
-
-//      var emcode;
-//      var emid;
-
-//      $('.embed-textarea').bind('mouseover', function(){
-//        emcode = this.value;
-//        emid = $(this).attr('id');
-//        this.select();
-//      });
-//
-//      $('.embed-textarea').keyup(function(){
-//        this.value = emcode;
-//        this.select();
-//      });
-//
-//      /* ZeroClipboard https://github.com/zeroclipboard/ZeroClipboard */
-//      var hasFlash = false; try { var fo = (navigator.mimeTypes && navigator.mimeTypes['application/x-shockwave-flash']) ? navigator.mimeTypes['application/x-shockwave-flash'].enabledPlugin : 0; if(fo) hasFlash = true; }catch(e){ if(navigator.mimeTypes ['application/x-shockwave-flash'] != undefined) hasFlash = true; }
-//      if (hasFlash) { //only proceed if Flash is enabled
-//        ZeroClipboard.setDefaults( { moviePath: 'http://' + location.hostname + '/sites/all/libraries/zeroclipboard/ZeroClipboard.swf' } );
-//        var clipsmall = new ZeroClipboard($("#embed-textarea-small"));
-//        clipsmall.glue( document.getElementById('embed-textarea-small') );
-//        //all 3 clips call this function when complete
-//        clipsmall.on( 'complete', function ( client, args ) {
-//          $('#zeroclipmessage').text("Copied " + emid.substr(15) + " embed code to clipboard: " + args.text);
-//        });
-//        var clipmedium = new ZeroClipboard($("#embed-textarea-medium"));
-//        clipmedium.glue( document.getElementById('embed-textarea-medium') );
-//        var cliplarge = new ZeroClipboard($("#embed-textarea-large"));
-//        cliplarge.glue( document.getElementById('embed-textarea-large') );
-//      }
     }
   };
 
