9da6dd841e730aeda19ca3c310827cc97d6ab9db
teknic <btate340@gmail.com>
2013-12-06 11:45:53 -0800

    updated

---
+++ docroot/sites/all/modules/custom/sbac_content_types/sbac_resource/templates/related_resources/related-resources-header.tpl.php
@@ -0,0 +1,3 @@
+<h1>Related Resources</h1>
+<div class="related-resources-container">Related resources are displayed below based on their relationship to this resource. Factors used to identify related resources include similar
+  subjects, grade levels and attributes of a formative assessment process. Resources that have better reviews or more views are displayed first.</div>
