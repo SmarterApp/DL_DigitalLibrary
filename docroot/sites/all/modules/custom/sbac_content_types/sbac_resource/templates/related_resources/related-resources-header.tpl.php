daafed09f4346e3d3ea827d20780e091eb22aa1f
limage <li@appnovation.com>
2013-12-06 12:40:13 -0800

    related resources page

--- docroot/sites/all/modules/custom/sbac_content_types/sbac_resource/templates/related_resources/related-resources-header.tpl.php
+++ docroot/sites/all/modules/custom/sbac_content_types/sbac_resource/templates/related_resources/related-resources-header.tpl.php
@@ -1,3 +1,5 @@
-<h1>Related Resources</h1>
-<div class="related-resources-container">Related resources are displayed below based on their relationship to this resource. Factors used to identify related resources include similar
-  subjects, grade levels and attributes of a formative assessment process. Resources that have better reviews or more views are displayed first.</div>
+<div class="related-resources">
+	<h2>Related Resources</h2>
+	<div class="related-resources-container"><p>Related resources are displayed below based on their relationship to this resource. Factors used to identify related resources include similar
+	  subjects, grade levels and attributes of a formative assessment process. Resources that have better reviews or more views are displayed first.</p></div>
+</div>
