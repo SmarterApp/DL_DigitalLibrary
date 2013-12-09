13f78df939b317e190d87485257b3fe7011bda68
teknic <btate340@gmail.com>
2013-12-09 12:57:45 -0800

    added flexslider

---
+++ docroot/sites/all/modules/custom/sbac_content_types/sbac_resource/js/sbac_resource.related_resources.js
@@ -0,0 +1,47 @@
+(function($) {
+
+  if (typeof SBAC_Related_Resource == 'undefined') {
+    SBAC_Related_Resource = {};
+  }
+
+  SBAC_Related_Resource.related_resources = {
+    slider: '.section-5 .resources-container',
+    slider_child: '.slides li',
+    slider_width: 413,
+
+    init_flexslider : function() {
+      if ((slider = $(SBAC_Related_Resource.related_resources.slider)).length) {
+        $(slider).css('width', '820px');
+        slider.flexslider({
+          animation: "slide",
+          animationLoop: false,
+          itemMargin: 0,
+          itemWidth: SBAC_Related_Resource.related_resources.slider_width,
+          slideshow: false
+        });
+        $(SBAC_Related_Resource.related_resources.slider_child, slider).css('width', SBAC_Related_Resource.related_resources.slider_width + 'px');
+      }
+    }
+  };
+
+  Drupal.behaviors.SBAC_Related_Resources = {
+    attach: function (context, settings) {
+      SBAC_Related_Resource.related_resources.init_flexslider();
+      // we need to re-initialize the flexslider when a user clicks on the related resources tab
+      var section = $('#review .section-container .section-5');
+      if (section.length) {
+        section.bind('classAdded', function(e, css_class) {
+          if (css_class == 'active') {
+            SBAC_Related_Resource.related_resources.init_flexslider();
+          }
+        });
+      }
+      // we also need to re-init the slder when a user leaves the browser window and
+      // comes back to it, because the slider occasionally chooses to go on vacation
+      $(window).focus(function() {
+        SBAC_Related_Resource.related_resources.init_flexslider();
+      });
+    }
+  };
+
+})(jQuery);
