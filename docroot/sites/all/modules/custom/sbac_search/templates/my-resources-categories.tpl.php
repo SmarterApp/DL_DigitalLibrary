a3d8e64a225db3c18d248a86aae650ae0c76541e
Afraaz Ali <afraaz@appnovation.com>
2014-01-08 11:17:44 -0800

    remove selected categories and clear all button

--- docroot/sites/all/modules/custom/sbac_search/templates/my-resources-categories.tpl.php
+++ docroot/sites/all/modules/custom/sbac_search/templates/my-resources-categories.tpl.php
@@ -59,10 +59,6 @@
   </div>
   <?php ($cf_value ? $class = '' : $class = 'noshow'); ?>
   <div class="categories-current-filters clearfix <?php echo $class; ?>" id="sbac-category-current-filters">
-    <div class="categories-clear-all noshow right">
-      <a class="categories-clear-all-button button tiny">Clear All</a>
-    </div>
-    <h3>Selected Categories</h3>
     <?php
     if ($cf_html) {
       foreach ($cf_html as $current_filter_html) {
