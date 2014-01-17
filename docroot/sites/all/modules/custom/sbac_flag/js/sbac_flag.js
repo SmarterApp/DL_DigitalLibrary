98f18611ab171a40bd169443cbbbca9d0f04382f
teknic <btate340@gmail.com>
2014-01-17 12:38:10 -0800

    added template, taskit object, modified notifications and modal functionality

--- docroot/sites/all/modules/custom/sbac_flag/js/sbac_flag.js
+++ docroot/sites/all/modules/custom/sbac_flag/js/sbac_flag.js
@@ -3,13 +3,17 @@
 
   Drupal.behaviors.sbac_flag = {
     attach: function (context, settings) {
+      // Hide modal button.
       $('#sbac-flag-resource-modal').hide();
 
+      // If there is an error, click the radio button so that the
+      // textarea moves to the right location.
       if ($('input[name=flag_options]:radio:checked')) {
         var radio = $('input[name=flag_options]:radio:checked');
         radio.click();
       }
 
+      // On click of radio button, move textarea.
       $('#sbac-flag-options input').click( function() {
         $(this).parent().append($('#sbac-flag-optional-comment'));
         $('#sbac-flag-optional-comment').show();
