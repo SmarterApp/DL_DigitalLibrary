28353e62673976cbf91987db15c8c6e0cdccccd2
teknic <btate340@gmail.com>
2014-01-16 18:41:32 -0800

    added flag module

---
+++ docroot/sites/all/modules/custom/sbac_flag/js/sbac_flag.js
@@ -0,0 +1,18 @@
+(function ($) {
+  Drupal.behaviors = Drupal.behaviors || {};
+
+  Drupal.behaviors.sbac_flag = {
+    attach: function (context, settings) {
+      if ($('input[name=flag_options]:radio:checked')) {
+        var radio = $('input[name=flag_options]:radio:checked');
+        radio.click();
+      }
+
+      $('#sbac-flag-options input').click( function() {
+        $(this).parent().append($('#sbac-flag-optional-comment'));
+        $('#sbac-flag-optional-comment').show();
+      });
+    }
+  };
+
+})(jQuery);
