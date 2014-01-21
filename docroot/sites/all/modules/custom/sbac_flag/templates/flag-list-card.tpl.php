2611f3aaecea3c8ae820f71d9232c520a2ab3f16
teknic <btate340@gmail.com>
2014-01-20 16:14:57 -0800

    flag updates

---
+++ docroot/sites/all/modules/custom/sbac_flag/templates/flag-list-card.tpl.php
@@ -0,0 +1,19 @@
+<div class="resource-card">
+  <div class="row">
+    <?php if (isset($node->eck_review)): ?>
+      <div class="review-title"><a href="<?php echo $node->path['alias']; ?>"><?php echo $node->eck_review_title; ?></a></div>
+      <div class="resource-title">
+        <?php echo htmlspecialchars_decode($node->title); ?>
+      </div>
+    <?php endif; ?>
+    <?php if (!isset($node->eck_review)): ?>
+      <div class="resource-title">
+        <a href="<?php echo $node->path['alias']; ?>" ><?php echo htmlspecialchars_decode($node->title); ?></a>
+      </div>
+    <?php endif; ?>
+    <div class="flag-status">
+      <?php echo $node->eck_flag->display_status; ?>
+    </div>
+    <?php echo $node->eck_flag->button; ?>
+  </div>
+</div>
---
+++ docroot/sites/all/modules/custom/sbac_flag/templates/flag-moderation.tpl.php
@@ -0,0 +1,3 @@
+<div class="sbac-flag-moderation">
+  <?php echo $list_output; ?>
+</div>
