fe0edf00da13556a6ce59f93647186960a898fd8
teknic <btate340@gmail.com>
2014-01-24 16:03:57 -0800

    updated moderation queue

--- docroot/sites/all/modules/custom/sbac_flag/templates/flag-list-card.tpl.php
+++ docroot/sites/all/modules/custom/sbac_flag/templates/flag-list-card.tpl.php
@@ -1,25 +1,25 @@
 <div class="resource-card">
   <div class="row">
-    <?php if (isset($node->eck_review)): ?>
-      <div class="review-title"><a href="<?php echo $node->path['alias']; ?>"><?php echo $node->eck_review_title; ?></a></div>
+    <?php if (isset($flag_content['eck_review']['title'])): ?>
+      <div class="review-title"><a href="<?php echo $node->path['alias']; ?>"><?php echo $flag_content['eck_review']['title']; ?></a></div>
       <div class="resource-title">
         <?php echo htmlspecialchars_decode($node->title); ?>
       </div>
     <?php endif; ?>
-    <?php if (!isset($node->eck_review)): ?>
+    <?php if (!isset($flag_content['eck_review'])): ?>
       <div class="resource-title">
         <a href="<?php echo $node->path['alias']; ?>" ><?php echo htmlspecialchars_decode($node->title); ?></a>
       </div>
     <?php endif; ?>
 		<div class="row-right">
         <?php $class = ''; ?>
-        <?php if (isset($node->eck_flag->display_status_css) && $node->eck_flag->display_status_css == 'urgent'): ?>
+        <?php if (isset($flag_content['eck_flag']['display_status_css']) && $flag_content['eck_flag']['display_status_css'] == 'urgent'): ?>
           <?php $class = 'urgent'; ?>
         <?php endif; ?>
     		<div class="flag-status <?php echo $class; ?>">
-		      <?php echo $node->eck_flag->display_status; ?>
+		      <?php echo $flag_content['eck_flag']['display_status']; ?>
 		    </div>
-		    <?php echo $node->eck_flag->button; ?>
+		    <?php echo $flag_content['eck_flag']['button']; ?>
 		</div>
   </div>
 </div>
