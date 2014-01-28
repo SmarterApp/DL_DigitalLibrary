0ce52fc99d83ea5ef7974d5955a6f27967bbdb3f
teknic <btate340@gmail.com>
2014-01-28 12:00:44 -0800

    fixed issue with selector ID changing

--- docroot/sites/all/modules/custom/sbac_content_types/sbac_resource/js/sbac_resource.form.js
+++ docroot/sites/all/modules/custom/sbac_content_types/sbac_resource/js/sbac_resource.form.js
@@ -183,7 +183,8 @@
         clicked = true;
       });
 
-      $('#edit-finish-moderation').click( function () {
+      //$('#edit-finish-moderation').click( function () {
+      $("[id^=edit-finish-moderation]").click( function () {
         isDirty = false;
       });
     }
