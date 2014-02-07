eb9efc74993959c1df6509d221b24c9128de713e
teknic <btate340@gmail.com>
2014-02-07 11:56:15 -0800

    updated js

--- docroot/sites/all/modules/custom/sbac_report/js/sbac_report.csv.js
+++ docroot/sites/all/modules/custom/sbac_report/js/sbac_report.csv.js
@@ -3,28 +3,12 @@
 
   Drupal.behaviors.sbac_report_csv = {
     attach: function (context, settings) {
-      function updateLabel(label_for, textfield_id) {
-        var label = $('label[for=' + label_for + ']');
-        var textfield = $('#' + textfield_id + ' input[type=text]');
-        if (label.length && textfield.length) {
-          label.attr('for', label_for + '_text');
-          textfield.attr('id', label_for + '_text');
-        }
-      }
-
-      function addDeleteButtonTitles(elm) {
-        $(elm).find('li').each(function (i, v) {
-          var selection = $(v).find('span').text();
-          $(v).find('a').attr('title', 'Delete ' + selection);
-        });
-      }
-
       // Associate the "Choose a state" label with the text field so that screen readers speak the label.
-      updateLabel('edit-state--2', 'edit_state__2_chzn');
+      Drupal.behaviors.sbac_report_csv.updateLabel('edit-state--2', 'edit_state__2_chzn');
       // Associate the "Choose User Groups" label with the text field so that screen readers speak the label.
-      updateLabel('edit-user-group-dropdown', 'edit_user_group_dropdown_chzn');
+      Drupal.behaviors.sbac_report_csv.updateLabel('edit-user-group-dropdown', 'edit_user_group_dropdown_chzn');
       // Associate the "Filter for SNE or SLT Members" label with the text field so that screen readers speak the label.
-      updateLabel('edit-sne-slt-filter', 'edit_sne_slt_filter_chzn');
+      Drupal.behaviors.sbac_report_csv.updateLabel('edit-sne-slt-filter', 'edit_sne_slt_filter_chzn');
 
       $.each([
         '#edit_state__2_chzn', '#edit_user_group_dropdown_chzn', '#edit_sne_slt_filter_chzn',
@@ -32,15 +16,35 @@
         '#edit_resource_subject_dropdown_chzn',
         '#edit_resource_grade_dropdown_chzn', '#edit_resource_attribute_dropdown_chzn'
       ], function (i, v) {
-
         // Add a title attribute to the delete tags on the User Activity and Detailed Resource tabs.
-        addDeleteButtonTitles(v);
+        Drupal.behaviors.sbac_report_csv.addDeleteButtonTitles(v);
         $(v + ' ul.chzn-choices').bind('DOMSubtreeModified', function (e) {
-          addDeleteButtonTitles(v);
+          alert('hi');
+          Drupal.behaviors.sbac_report_csv.addDeleteButtonTitles(v);
         });
 
         // Add aria-required attribute to required fields.
         $(v + ' input[type=text').attr('aria-required', 'true');
+      });
+    },
+    updateLabel: function (label_for, textfield_id) {
+      var label = $('label[for=' + label_for + ']');
+      var textfield = $('#' + textfield_id + ' input[type=text]');
+      if (label.length && textfield.length) {
+        label.attr('for', label_for + '_text');
+        textfield.attr('id', label_for + '_text');
+      }
+    },
+    addDeleteButtonTitles: function (elm) {
+      var selection = null;
+      $(elm).find('li').each(function (i, v) {
+        selection = $(v).find('span').text();
+        if (selection != "" && selection != null) {
+          selection = 'Delete ' + selection;
+          if ($(v).find('a').length) {
+            $(v).find('a').attr('title', selection);
+          }
+        }
       });
     }
   };
