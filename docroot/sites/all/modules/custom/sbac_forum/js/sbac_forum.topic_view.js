714b9c955998196ee563a89beeee43a806f1b8fb
jwhang <jonathan@appnovation.com>
2014-03-07 16:41:18 -0800

    Forum - Added inline comment form

--- docroot/sites/all/modules/custom/sbac_forum/js/sbac_forum.topic_view.js
+++ docroot/sites/all/modules/custom/sbac_forum/js/sbac_forum.topic_view.js
@@ -14,9 +14,35 @@
       });
 
 
-    }
+      var comment_open = function(e) {
+        //remove all already open comment forms
+        $(".topic-node-comment-region-right-reply-form").each(function() {
+          $(this).hide('fast');
+          $(this).remove(".comment-form");
+        });
+
+        var title = $(this).attr('title');
+        var href = $(this).attr('href');
+        var formCont = $(this).parents("[class~=topic-node-comment-region-right-bottom]").next();
+
+        // Load the page callback contents (form). Re-attach all behaviours
+        formCont.load(href,function(){
+          Drupal.behaviors.sbac_forum_js_topic_node_view_page.expand(formCont);
+          Drupal.attachBehaviors(formCont, Drupal.settings);
+        });
+
+        return false;
+      };
+
+      $(".comment-reply a, .comment-add a").once('comment-click', function() {
+        $(this).click(comment_open);
+      });
+    }, // End attach
+    expand: function(formCont) {
+      formCont.show('slow');
+      formCont.animate({
+        'min-height': ['220px', "swing"],
+      });
+    } // End expand.
   };
-
-
-
 })(jQuery);
---
+++ docroot/sites/all/themes/SBAC/templates/page--sbac-forum--custom--comment-reply.tpl.php
@@ -0,0 +1,8 @@
+
+<div class="inline-comment-page">
+  <?php if ($messages): print $messages; endif; ?>
+  <?php print render($page['content']); ?>
+</div>
+
+
+
