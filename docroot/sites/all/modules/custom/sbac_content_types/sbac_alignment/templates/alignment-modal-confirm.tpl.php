c9a8246735b1750ee6bdd7252a8cb43a359c3e3d
teknic <btate340@gmail.com>
2014-04-01 15:08:33 -0700

    added titles and # to empty hrefs

--- docroot/sites/all/modules/custom/sbac_content_types/sbac_alignment/templates/alignment-modal-confirm.tpl.php
+++ docroot/sites/all/modules/custom/sbac_content_types/sbac_alignment/templates/alignment-modal-confirm.tpl.php
@@ -2,7 +2,7 @@
   <h2>Add Selected Standards?</h2>
   <p>Unsaved selections will be discarded if you proceed</p>
   <div class="confirm-buttons">
-    <a href="" id="cmod-confirm-cancel" class="button bc-discard-selections">Discard Selections</a>
-    <a href="" id="cmod-confirm-submit" class="button">Add Selections</a>
+    <a href="#" title="Discard Selections" id="cmod-confirm-cancel" class="button bc-discard-selections">Discard Selections</a>
+    <a href="#" title="Add Selections" id="cmod-confirm-submit" class="button">Add Selections</a>
   </div>
 </div>
--- docroot/sites/all/modules/custom/sbac_content_types/sbac_help/templates/help-drop-down.tpl.php
+++ docroot/sites/all/modules/custom/sbac_content_types/sbac_help/templates/help-drop-down.tpl.php
@@ -1,5 +1,5 @@
 <div class="help-dropdown">
-    <a  data-dropdown="drop1" href=""><span class="sbac-question"></span> Help</a>
+    <a  data-dropdown="drop1" href="#" title="Help"><span class="sbac-question"></span> Help</a>
     <ul id="drop1" class="f-dropdown" data-dropdown-content>
         <li><a href="#helpmodal" class="help-modal">Welcome Tutorial</a></li>
         <li><?php print l(t('Glossary'), 'glossary', array('absolute' => TRUE)); ?></li>
