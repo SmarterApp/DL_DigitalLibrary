c81c4ed58854de79d1fa93e2d473955ce345f895
seantran <seant@appnovation.com>
2014-04-03 11:29:40 -0700

    Text change

--- docroot/sites/all/modules/custom/sbac_learning_registry/templates/external-resource.tpl.php
+++ docroot/sites/all/modules/custom/sbac_learning_registry/templates/external-resource.tpl.php
@@ -1,7 +1,6 @@
 <div>
   <div>
-    <div>External Resources</div>
-    <div>These resources have not been reviewed by Smarter Balanced</div>
+    <div>This is a Digital Library experimental feature. Smarter Balanced does not review or evaluate the quality of these resources</div>
   </div>
   <div>
     <?php print $results; ?>
