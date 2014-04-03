21adeb8f8b8bd27106b337aee883263a156eb0b1
limage <li@appnovation.com>
2014-04-03 15:24:32 -0700

    search box

--- docroot/sites/all/modules/custom/sbac_learning_registry/templates/external-resource-results.tpl.php
+++ docroot/sites/all/modules/custom/sbac_learning_registry/templates/external-resource-results.tpl.php
@@ -1,7 +1,7 @@
 <?php foreach ($results as $result): ?>
   <div class="ex-resource-card">
     <div class="resource-title">
-			<a href="<?php print $result['URL']; ?>" target="_blank"><?php print truncate_utf8($result['name'], 54, TRUE, TRUE); ?></a>
+			<a href="<?php print $result['URL']; ?>" target="_blank"><?php print truncate_utf8($result['name'], 34, TRUE, TRUE); ?></a>
 		</div>
     <div class="resource-content">
 			<?php print truncate_utf8($result['description'], 184, TRUE, TRUE); ?>
