67d4ca23b3264f31c7ec5f4a0a387623978d352d
limage <li@appnovation.com>
2014-04-03 11:15:56 -0700

    resource card list theming

--- docroot/sites/all/modules/custom/sbac_learning_registry/templates/external-resource-results.tpl.php
+++ docroot/sites/all/modules/custom/sbac_learning_registry/templates/external-resource-results.tpl.php
@@ -1,7 +1,11 @@
 <?php foreach ($results as $result): ?>
-  <div>
-    <div><a href="<?php print $result['URL']; ?>" target="_blank"><?php print $result['name']; ?></a></div>
-    <div><?php print $result['description']; ?></div>
+  <div class="ex-resource-card">
+    <div class="resource-title">
+			<a href="<?php print $result['URL']; ?>" target="_blank"><?php print truncate_utf8($result['name'], 54, TRUE, TRUE); ?></a>
+		</div>
+    <div class="resource-content">
+			<?php print truncate_utf8($result['description'], 184, TRUE, TRUE); ?>
+		</div>
   </div>
 <?php endforeach; ?>
 <?php print $load_more_link; ?>
