85d5c248946aeedaf562b0ca5ee75e3eedb663b7
Paolo Montano <Paolo@Appnovation.com>
2014-04-04 15:55:09 -0700

    508 fix for type needed for script in HTML.

--- docroot/sites/all/themes/SBAC/templates/html.tpl.php
+++ docroot/sites/all/themes/SBAC/templates/html.tpl.php
@@ -84,7 +84,7 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
-  <script>
+  <script type='text/javascript'>
     (function ($, Drupal, window, document, undefined) {
       $(document).foundation();
     })(jQuery, Drupal, this, this.document);
