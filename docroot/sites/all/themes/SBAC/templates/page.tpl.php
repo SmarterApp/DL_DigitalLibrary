a4e8ec39640df9d787384972a33396f2796c507f
teknic <btate340@gmail.com>
2014-04-03 17:19:53 -0700

    added more 508

--- docroot/sites/all/themes/SBAC/templates/page.tpl.php
+++ docroot/sites/all/themes/SBAC/templates/page.tpl.php
@@ -245,7 +245,7 @@ global $user;
         <li>
           <?php if (user_is_logged_in() && !in_array('guest', $user->roles)) : ?>
           <div class="footer-help">
-            <a class="help help-dropdown-footer" data-dropdown="drop2" href="#"><span class="sbac-question"></span> Help</a>
+            <a title="Help Menu" class="help help-dropdown-footer" data-dropdown="drop2" href="#"><span class="sbac-question"></span> Help</a>
             <ul id="drop2" class="f-dropdown" data-dropdown-content>
               <li><a title="Welcome Tutorial" href="#helpmodal" class="help-modal">Welcome Tutorial</a></li>
               <li><?php print l(t('Glossary'), 'glossary', array('absolute' => TRUE, 'attributes' => array('title' => 'Glossary'))); ?></li>
