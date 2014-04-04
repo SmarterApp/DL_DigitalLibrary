6026cca9aecfa044bdd42333ed1110899ed3b6cf
seantran <seant@appnovation.com>
2014-04-04 10:45:49 -0700

    Added default and no search results pages

--- docroot/sites/all/modules/custom/sbac_learning_registry/templates/external-resource.tpl.php
+++ docroot/sites/all/modules/custom/sbac_learning_registry/templates/external-resource.tpl.php
@@ -2,7 +2,21 @@
   <div class="feature-message">
     <p>This is a Digital Library experimental feature. Smarter Balanced does not review or evaluate the quality of these resources.</p>
   </div>
-  <div>
-    <?php print $results; ?>
-  </div>
+  <?php if (!empty($results) && $results != 'no results'): ?>
+    <div>
+      <?php print $results; ?>
+    </div>
+  <?php elseif ($results == 'no results'): ?>
+    <div>
+      <h2>Your search returned no results.</h2>
+      <p>Please try a different search term.</p>
+    </div>
+  <?php
+  else: ?>
+    <div>
+      <p>You may use the keyword search and filters above to search for external resources that are in the Learning Registry.</p>
+
+      <p>The Learning Registry is a searchable set of external online education-related resources from participating libraries.</p>
+    </div>
+  <?php endif; ?>
 </div>
