2524458abfbc3212456b4a5ddfa47d846cda2ed5
seantran <seant@appnovation.com>
2014-05-05 12:50:42 -0700

    Collaboration - Update to the forum layout

--- docroot/sites/all/modules/custom/sbac_forum/templates/forum-view-page.tpl.php
+++ docroot/sites/all/modules/custom/sbac_forum/templates/forum-view-page.tpl.php
@@ -1,84 +1,68 @@
 <div class="sbac-forum-vp-controls-wrapper">
-  <?php if (!empty($fields['controls_link_list']['markup']) ): ?>
+  <?php if (!empty($fields['controls_link_list']['markup'])): ?>
     <div class="sbac-forum-vp-controls">
-      <?php print t('Forum Settings');?>
+      <?php print t('Forum Settings'); ?>
     </div>
     <div class="sbac-forum-vp-controls-popup <?php print $fields['controls_link_list']['classes']; ?>">
       <?php print $fields['controls_link_list']['markup']; ?>
     </div>
-  <?php endif;?>
+  <?php endif; ?>
 </div>
 <div class="sbac-forum-confirm-message-region">
   <?php if (!empty($fields['welcome_message'])) : ?>
-      <div style="display:block">
-        <?php print $fields['welcome_message']; ?>
-      </div>
-  <?php endif;?>
+    <div style="display:block">
+      <?php print $fields['welcome_message']; ?>
+    </div>
+  <?php endif; ?>
 </div>
-
 <div class="sbac-forum-vp-top clearfix">
-
-  <div class="col narrow">
-
-    <?php if ( !empty($fields['description']) ): ?>
-      <div class="vp-desc-field">
-        <?php print $fields['description']; ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if (!empty($fields['facilitators']) ): ?>
-      <div class="vp-faci-field">
-        <div class="title-tags"><?php print t('Facilitator(s):'); ?></div>
-        <?php print $fields['facilitators'];?>
-      </div>
-    <?php endif;?>
-
-  </div> <!-- End top-left-->
-
-
-  <div class="col narrow">
-
-    <?php if (!empty($fields['grades'])):?>
-      <div class="vp-grades-field">
-        <div class="title-tags"><?php print t('Grades'); ?></div>
-        <?php print $fields['grades']; ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if (!empty($fields['subjects'])):?>
-      <div class="vp-subjects-field">
-        <div class="title-tags"><?php print t('Subjects and Domains'); ?></div>
-        <?php print $fields['subjects']; ?>
-      </div>
-    <?php endif; ?>
-
-  </div> <!-- End top-center-->
-
-
-  <div class="col narrow">
-
-    <?php if (!empty($fields['end_users'])):?>
-      <div class="vp-end-users-field">
-        <div class="title-tags"><?php print t('Intended Forum Participants'); ?></div>
-        <?php print $fields['end_users']; ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if (!empty($fields['students'])):?>
-      <div class="vp-students-field">
-        <div class="title-tags"><?php print t('Intended Student Populations'); ?></div>
-        <?php print $fields['students']; ?>
-      </div>
-    <?php endif; ?>
-
-  </div> <!-- End top-right-->
-
-</div> <!--End top container -->
-
-<?php if(isset($fields['favorites_link'])): ?>
-  <?php print $fields['favorites_link']; ?>
-<?php endif; ?>
-
+  <?php if (!empty($fields['description'])): ?>
+    <div class="vp-desc-field">
+      <?php print $fields['description']; ?>
+    </div>
+  <?php endif; ?>
+  <?php if (!empty($fields['facilitators'])): ?>
+    <div class="vp-faci-field">
+      <div class="title-tags"><?php print t('Facilitator(s):'); ?></div>
+      <?php print $fields['facilitators']; ?>
+    </div>
+  <?php endif; ?>
+  <?php if (!empty($fields['grades'])): ?>
+    <div class="vp-grades-field">
+      <div class="title-tags"><?php print t('Grades'); ?></div>
+      <?php print $fields['grades']; ?>
+    </div>
+  <?php endif; ?>
+  <?php if (!empty($fields['subjects'])): ?>
+    <div class="vp-subjects-field">
+      <div class="title-tags"><?php print t('Subjects and Domains'); ?></div>
+      <?php print $fields['subjects']; ?>
+    </div>
+  <?php endif; ?>
+  <?php if (!empty($fields['end_users'])): ?>
+    <div class="vp-end-users-field">
+      <div class="title-tags"><?php print t('Intended Forum Participants'); ?></div>
+      <?php print $fields['end_users']; ?>
+    </div>
+  <?php endif; ?>
+  <?php if (!empty($fields['students'])): ?>
+    <div class="vp-students-field">
+      <div class="title-tags"><?php print t('Intended Student Populations'); ?></div>
+      <?php print $fields['students']; ?>
+    </div>
+  <?php endif; ?>
+  <?php if (isset($fields['favorites_link'])): ?>
+    <?php print $fields['favorites_link']; ?>
+  <?php endif; ?>
+  <?php if (!empty($fields['resource_link'])): ?>
+    <div>
+      <?php print $fields['resource_link']; ?>
+    </div>
+  <?php endif; ?>
+  <?php if (!empty($fields['resource_image'])): ?>
+    <?php print $fields['resource_image']; ?>
+  <?php endif; ?>
+</div>
 <div class="sbac-forum-vp-tabs">
   <?php print $fields['tabs']; ?>
 </div>
