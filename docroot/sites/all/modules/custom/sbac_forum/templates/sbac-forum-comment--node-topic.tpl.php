ec9fa3ab5a5f601f34f5d2d248778356cf75653f
limage <li@appnovation.com>
2014-05-09 16:03:28 -0700

    flag theming

--- docroot/sites/all/modules/custom/sbac_forum/templates/sbac-forum-comment--node-topic.tpl.php
+++ docroot/sites/all/modules/custom/sbac_forum/templates/sbac-forum-comment--node-topic.tpl.php
@@ -117,8 +117,10 @@
 	    </div>
 	    <div class="topic-node-comment-region-right-bottm-comment flagged-post-for-<?php print $comment->cid; ?>">
 	      <?php if (!empty($content['flags']['comments']) && $is_member === TRUE): ?>
+				<div class='flag-section'>
 	        <?php print render($content['flags']['comments']['reason']); ?>
 	        <?php print render($content['flags']['comments']['comment']); ?>
+				</div>
 	      <?php endif; ?>
 	    </div>
 	    <div class="topic-node-comment-region-right-reply-form">
