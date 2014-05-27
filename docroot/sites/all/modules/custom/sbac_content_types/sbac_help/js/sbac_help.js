360ddaa96fb10812dbfe4f68713d711b27e7f433
teknic <btate340@gmail.com>
2014-05-26 20:15:22 -0700

    updates

--- docroot/sites/all/modules/custom/sbac_content_types/sbac_help/js/sbac_help.js
+++ docroot/sites/all/modules/custom/sbac_content_types/sbac_help/js/sbac_help.js
@@ -1,14 +1,14 @@
 (function ($) {
   Drupal.behaviors = Drupal.behaviors || {};
 
-  Drupal.behaviors.flexslider = {
+  Drupal.behaviors.sbac_help_main = {
     attach: function (context, settings) {
       $('#sbac-help-welcome-tutorial').hide();
       $('.sbac-help-welcome-tutorial-modal').click( function() {
         $('#sbac-help-welcome-tutorial').click();
       });
 
-      jQuery(".flexslider").flexslider({
+      $(".flexslider").flexslider({
         animation: "slide",
         animationLoop: false,
         itemMargin: 0,
@@ -18,52 +18,47 @@
     }
   };
 
-  Drupal.behaviors.sbac_help_area = {
-//    attach: function (context, settings) {
-//      $('#helpmodal .views-field-field-help-icon a').click(function (e) {
-//        var nid = $(this).parents('.views-field-field-help-icon').next('.views-field-title').next('.views-field-nid').find('.field-content').html();
-//        sbacHelpAjax(nid);
-//        return false;
-//      });
-//
-//      $('#helpmodal .views-field-title a').click(function (e) {
-//        var nid = $(this).parents('.views-field-title').next('.views-field-nid').find('.field-content').html();
-//        sbacHelpAjax(nid);
-//        return false;
-//      });
-//
-//     function initTextify(){
-//      jQuery('#current-help-topic')
-//        .textify({
-//          numberOfColumn: 2,
-//          margin: 20,
-//          padding: 32,
-//          width: "auto",
-//          height: "400",
-//          showArrows : true,
-//          showNavigation: true
-//        });
-//      }
+  Drupal.behaviors.sbac_help_modal = {
+    attach: function (context, settings) {
+      $('#modal-content .views-field-field-help-icon a').click(function (e) {
+        var nid = $(this).parents('.views-field-field-help-icon').next('.views-field-title').next('.views-field-nid').find('.field-content').html();
+        sbacHelpAjax(nid);
+        return false;
+      });
 
-//      function sbacHelpAjax(nid) {
-//        $.ajax({
-//          url: Drupal.settings.basePath + 'get-node-content',
-//          data: {nid: nid},
-//          success: function (data, textStatus, jqXHR) {
-//            $('#current-help-topic').html(data.results.body);
-//            $('#sbac-help-title').html(data.results.title);
-//            $.colorbox({
-//              open:true,
-//              inline:true,
-//              href: '#current-help-topic-modal',
-//              width:"80%",
-//              height:"600px",
-//              onComplete:initTextify
-//            });
-//          }
-//        });
-//      }
-//    }
+      $('#modal-content .views-field-title a').click(function (e) {
+        var nid = $(this).parents('.views-field-title').next('.views-field-nid').find('.field-content').html();
+        sbacHelpAjax(nid);
+        return false;
+      });
+
+     function initTextify(){
+      $('.sbac-help-node-content')
+        .textify({
+          numberOfColumn: 2,
+          margin: 20,
+          padding: 32,
+          width: "auto",
+          height: "400",
+          showArrows : true,
+          showNavigation: true
+        });
+      }
+
+      function sbacHelpAjax(nid) {
+        $.ajax({
+          url: Drupal.settings.basePath + 'get-node-content',
+          data: {nid: nid},
+          success: function (data, textStatus, jqXHR) {
+            var response = jQuery.parseJSON(data);
+            $('.sbac-help-topics').hide();
+            $('.sbac-help-node-content').html(response.results.body);
+            $('.helpmodal-title').html(response.results.title);
+            initTextify();
+          }
+        });
+      }
+    }
   };
 
 })(jQuery);
