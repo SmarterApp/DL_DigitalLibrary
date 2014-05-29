b58fe985bd88b118ad232f6d424b2d5176bddbe1
Paolo Montano <Paolo@Appnovation.com>
2014-05-29 12:03:28 -0700

    Fix for truncate text.

--- docroot/sites/all/modules/custom/sbac_flag/templates/flag-list-card-topic.tpl.php
+++ docroot/sites/all/modules/custom/sbac_flag/templates/flag-list-card-topic.tpl.php
@@ -13,7 +13,7 @@
         </div>
         <div class="post-body">
           <span class="post-body-text">
-            <?php echo $flag_content['eck_post']['post_body']; ?>
+            <?php echo substr($flag_content['eck_post']['post_body'], 0, 180) . ' ...'; ?>
           </span>
         </div>
       </div>
