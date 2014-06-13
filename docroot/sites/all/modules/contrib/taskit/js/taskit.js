f4d56533e8613dea9687e83e0cf06e14189f362a
teknic <btate340@gmail.com>
2014-06-12 19:59:53 -0700

    added load more to taskit

--- docroot/sites/all/modules/contrib/taskit/js/taskit.js
+++ docroot/sites/all/modules/contrib/taskit/js/taskit.js
@@ -9,12 +9,18 @@
       if (load_more_button.length) {
         load_more_button.click( function() {
           if (ajax_request == null) {
+            var page = load_more_button.attr('page');
+            var role = load_more_button.attr('role');
+            var interval = load_more_button.attr('interval');
             ajax_request = $.ajax({
               type: 'POST',
               url: "/taskit-load-more",
-              data: {'page' : load_more_button.attr('page'), 'role' : load_more_button.attr('role')},
+              data: {'page' : page, 'role' : role, 'interval' : interval},
               success: function(data) {
                 var response = jQuery.parseJSON(data);
+                $('.taskit-interval.' + interval).append(response.result);
+                page++;
+                load_more_button.attr('page', page);
                 ajax_request = null;
               },
               error: function(data) {
