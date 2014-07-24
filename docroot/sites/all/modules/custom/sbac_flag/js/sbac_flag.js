283a9e8b0e048891f848bab698c4d8147f384f14
teknic <btate340@gmail.com>
2014-07-24 10:10:19 -0700

    temp commit

--- docroot/sites/all/modules/custom/sbac_flag/js/sbac_flag.js
+++ docroot/sites/all/modules/custom/sbac_flag/js/sbac_flag.js
@@ -32,11 +32,10 @@
       $('#sbac-load-more').once('sbac-load-more-click', function () {
         $(this).click( function() {
           // On click, hash the url
-          pager_count++;
+          var pager_count = $(this).attr('offset');
           window.location.hash = 'pager=' + pager_count;
-          var pager = $(this).attr('offset');
           clicked = true;
-          Drupal.behaviors.sbac_flag_load_more.load_more_content(pager);
+          Drupal.behaviors.sbac_flag_load_more.load_more_content(pager_count, true, false);
           return false;
         });
       });
@@ -44,24 +43,67 @@
       var hash = window.location.hash;
       if (hash != '' && !has_run_once && !clicked) {
         var pager = hash.replace('#pager=', '');
-        Drupal.behaviors.sbac_flag_load_more.load_more_content(pager);
+        Drupal.behaviors.sbac_flag_load_more.load_more_content(pager, false, true);
         has_run_once = true;
       }
+
+      // Move and resize the modalBackdrop on resize of the window
+      modalBackdropResize = function(){
+        // Get our heights
+        var docHeight = $(document).height();
+        var docWidth = $(document).width();
+        var winHeight = $(window).height();
+        if( docHeight < winHeight ) docHeight = winHeight;
+        // Apply the changes
+        $('#modalBackdrop').css('height', docHeight + 'px').css('width', docWidth + 'px').show();
+      };
+      $(window).bind('resize', modalBackdropResize);
     },
-    load_more_content: function(offset) {
+    load_more_content: function(offset, load_more, display_backdrop) {
       if (ajax_request == null) {
+        if (display_backdrop) {
+          // Get the docHeight and (ugly hack) add 50 pixels to make sure we dont have a *visible* border below our div
+          var docHeight = $(document).height() + 50;
+          var docWidth = $(document).width();
+          var winHeight = $(window).height();
+          var winWidth = $(window).width();
+          if( docHeight < winHeight ) docHeight = winHeight;
+
+          // Create CSS attributes
+          css = jQuery.extend({
+            position: 'absolute',
+            left: '0px',
+            margin: '0px',
+            background: '#000',
+            opacity: '.55'
+          }, {});
+
+          // Add opacity handling for IE.
+          css.filter = 'alpha(opacity=' + (100 * css.opacity) + ')';
+          $('body').append('<div id="modalBackdrop" style="z-index: 1000; display: block;"></div>');
+          $('#modalBackdrop').css(css).css('top', 0).css('height', docHeight + 'px').css('width', docWidth + 'px').show();
+        }
+
         ajax_request = $.ajax({
           type: 'POST',
           url: "/sbac-flag/load-more",
           data: {'offset' : offset},
           success: function(data) {
             var response = jQuery.parseJSON(data);
-            $('#sbac-flag-mod-cont').append(response.list_output);
+            if (load_more) {
+              $('#sbac-flag-mod-cont').append(response.list_output);
+            }
+            else {
+              $('#sbac-flag-mod-cont').replaceWith(response.list_output);
+            }
             $('#sbac-load-more').attr('offset', response.offset);
             if (response.remove_button == true) {
               $('#sbac-load-more').hide();
             }
             Drupal.attachBehaviors();
+            if (display_backdrop) {
+              $('#modalBackdrop').remove();
+            }
           },
           error: function(data) {
           }
