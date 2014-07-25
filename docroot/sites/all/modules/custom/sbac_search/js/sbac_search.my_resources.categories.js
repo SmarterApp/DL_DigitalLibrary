e27178a0c867f39739dd6dca053a264a62385277
teknic <btate340@gmail.com>
2014-07-24 20:57:21 -0700

    adding loading gif in, adjusted thumbnail code, added some more error statements in

--- docroot/sites/all/modules/custom/sbac_search/js/sbac_search.my_resources.categories.js
+++ docroot/sites/all/modules/custom/sbac_search/js/sbac_search.my_resources.categories.js
@@ -400,6 +400,13 @@
       if (hash != '' && !has_run_once && !clicked) {
         var pager = hash.replace('#pager=', '');
         if (ajax_request == null) {
+          if (self.pageYOffset) { // all except Explorer
+            var wt = self.pageYOffset;
+          } else if (document.documentElement && document.documentElement.scrollTop) { // Explorer 6 Strict
+            var wt = document.documentElement.scrollTop;
+          } else if (document.body) { // all other Explorers
+            var wt = document.body.scrollTop;
+          }
           // Get the docHeight and (ugly hack) add 50 pixels to make sure we dont have a *visible* border below our div
           var docHeight = $(document).height() + 50;
           var docWidth = $(document).width();
@@ -407,6 +414,7 @@
           var winWidth = $(window).width();
           if( docHeight < winHeight ) docHeight = winHeight;
 
+          // Create CSS attributes
           css = jQuery.extend({
             position: 'absolute',
             left: '0px',
@@ -415,8 +423,19 @@
             opacity: '.55'
           }, {});
 
-          $('body').append('<div id="modalBackdrop" style="z-index: 1000; display: block;"></div>');
+          // Add opacity handling for IE.
+          css.filter = 'alpha(opacity=' + (100 * css.opacity) + ')';
+          var img_location = '/sites/all/themes/sbac/images/foundation/orbit/loading.gif';
+          var img = '<img src="' + img_location + '" alt="Smiley face" height="42" width="42">';
+          $('body').append('<div id="modalBackdrop" style="z-index: 1000; display: block;"></div><div id="modalContent" style="z-index: 1001; position: absolute;">' + img + '</div>');
+          // Create our content div, get the dimensions, and hide it
+          var modalContent = $('#modalContent').css('top','-1000px');
+          var mdcTop = wt + ( winHeight / 2 ) - (  modalContent.outerHeight() / 2);
+          var mdcLeft = ( winWidth / 2 ) - ( modalContent.outerWidth() / 2);
           $('#modalBackdrop').css(css).css('top', 0).css('height', docHeight + 'px').css('width', docWidth + 'px').show();
+          modalContent.css({top: mdcTop + 'px', left: mdcLeft + 'px'});
+
+          // Make the request
           ajax_request = $.ajax({
             type: 'POST',
             url: "/sbac-resource/load-more",
@@ -444,6 +463,7 @@
               Drupal.attachBehaviors($('.row.digital-library'), settings);
               has_run_once = true;
               $('#modalBackdrop').remove();
+              $('#modalContent').remove();
             },
             error: function(data) {
             }
