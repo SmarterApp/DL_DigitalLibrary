5c5930d71aacf009f900e26a8abd87c019a54349
Paolo Montano <Paolo@Appnovation.com>
2014-07-31 16:23:07 -0700

    Stopped using #states.

--- docroot/sites/all/modules/custom/sbac_user/js/sbac_user.firstsetup.js
+++ docroot/sites/all/modules/custom/sbac_user/js/sbac_user.firstsetup.js
@@ -3,9 +3,26 @@
 
   Drupal.behaviors.sbac_user_setup_pi = {
     attach: function (context, settings) {
+
+      /**
+       * SBAC User Setup - Page 1
+       */
+
+       // On page load, if there is no selection, disable the submit button and
+       // put hoverover on top.
+      if ($('#edit-user-title').val() != '') {
+        $('#edit-next-form').prop('disabled', false);
+      }
+      else {
+        $('#edit-next-form').prop('disabled', true);
+        $('#sbac-setup-hover-pi').css('z-index', 9999);
+      }
+
+      //  When user hovers over invisible div, toggle the popup to show if the 
+      //  submit button is disabled.
       $('#sbac-setup-hover-pi').hover(
         function() {
-          if ($('#edit-next-form').hasClass('form-disabled')) {
+          if ($('#edit-next-form').prop('disabled')) {
             $('#pi-form-drop').addClass('open');
           }
           else {
@@ -13,21 +30,46 @@
           }
         },
         function() {
-          if ($('#edit-next-form').hasClass('form-disabled')) {
+          if ($('#edit-next-form').prop('disabled')) {
             $('#pi-form-drop').removeClass('open');
           }
         }
       );
 
+      //  When the user changes their title, enable/disable the submit button 
+      //  based on their choice.
       $('#edit-user-title').change(function() {
-        if ($('#edit-next-form').hasClass('form-disabled')) {
+        if ($('#edit-user-title').val() != '') {
+          $('#edit-next-form').prop('disabled', false);
+        }
+        else {
           $('#sbac-setup-hover-pi').css('z-index', 9999);
+          $('#edit-next-form').prop('disabled', true);
         }
       });
 
+      /**
+       * SBAC User Setup - Page 2
+       */
+
+      // On page load, if there are no selections, disable the submit button and
+      // put hoverover on top.
+      sbac_user_check_fields();
+      function sbac_user_check_fields() {
+        if ($('#edit_subject_options_chzn .search-choice').length == 0 || $('#edit_grade_options_chzn .search-choice').length == 0 || $('#edit_student_population_options_chzn .search-choice').length == 0) {
+          $('#edit-next').prop('disabled', true);
+          $('#sbac-setup-hover-expertise').css('z-index', 9999);
+        } else {
+          $('#edit-next').prop('disabled', false);
+          $('#sbac-setup-hover-expertise').css('z-index', -9999);
+        }
+      }
+
+      //  When user hovers over invisible div, toggle the popup to show if the 
+      //  submit button is disabled.
       $('#sbac-setup-hover-expertise').hover(
         function() {
-          if ($('#edit-next').hasClass('form-disabled')) {
+          if ($('#edit-next').prop('disabled')) {
             $('#expertise-form-drop').addClass('open');
           }
           else {
@@ -35,18 +77,28 @@
           }
         },
         function() {
-          if ($('#edit-next').hasClass('form-disabled')) {
+          if ($('#edit-next').prop('disabled')) {
             $('#expertise-form-drop').removeClass('open');
           }
         }
       );
 
-      $('#edit-is-complete').change(function() {
-        if ($('#edit-next').hasClass('form-disabled')) {
-          $('#sbac-setup-hover-pi').css('z-index', 9999);
-        }
+      $('#edit_subject_options_chzn :input').blur(function() {
+        sbac_user_check_fields();
+      });
+
+      $('#edit_grade_options_chzn :input').blur(function() {
+        sbac_user_check_fields();
+      });
+
+      $('#edit_student_population_options_chzn :input').blur(function() {
+        sbac_user_check_fields();
+      });
+
+      $('#edit-next').hover(function() {
+        sbac_user_check_fields();
       });
 
     }
-  };
+  }
 }) (jQuery);
