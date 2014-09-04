f21c7590e13c1e60a08fba0392b70f522717ac73
teknic <btate340@gmail.com>
2014-09-04 15:12:05 -0700

    removed google_cdn

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -581,36 +581,43 @@ if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
     case 'dev':
       $conf['file_private_path'] = '/mnt/files/sbacdev/files-private';
       $conf['google_cdn_folder'] = 'dev';
+      $conf['oscaddie_gcs_folder'] = 'dev';
       // do something on dev
       break;
     case 'test':
       // do something on staging
       $conf['file_private_path'] = '/mnt/files/sbacstg/files-private';
       $conf['google_cdn_folder'] = 'stage';
+      $conf['oscaddie_gcs_folder'] = 'stage';
       break;
     case 'stage2':
       // do something on staging2
       $conf['file_private_path'] = '/mnt/files/sbacstg2/files-private';
       $conf['google_cdn_folder'] = 'stage2';
+      $conf['oscaddie_gcs_folder'] = 'stage2';
       break;
     case 'stage3':
       // do something on staging3
       $conf['file_private_path'] = '/mnt/files/sbacstg3/files-private';
       $conf['google_cdn_folder'] = 'integration';
+      $conf['oscaddie_gcs_folder'] = 'integration';
       break;
     case 'prod':
       // do something on prod
       $conf['file_private_path'] = '/mnt/files/sbac/files-private';
       $conf['google_cdn_folder'] = 'production';
+      $conf['oscaddie_gcs_folder'] = 'production';
       break;
     case 'loadtest':
       $conf['file_private_path'] = '/mnt/files/sbacloadtest/files-private';
       $conf['google_cdn_folder'] = 'performance';
+      $conf['oscaddie_gcs_folder'] = 'performance';
       break;
   }
 }
 else {
   $conf['google_cdn_folder'] = 'local';
+  $conf['oscaddie_gcs_folder'] = 'local';
   $conf['file_default_scheme'] = 'private';
 }
 
@@ -622,7 +629,13 @@ $conf['google_cdn_service_account_name'] = '363452670531-qfh0aklr59p6h86jcq0hkgf
 $conf['google_cdn_signed_url_expiry'] = 300;
 $conf['google_cdn_library_version'] = 2;
 
-//ini_set('memory_limit', '196M');
+// osCaddie GCS variables.
+$conf['oscaddie_gcs_bucket_name'] = 'sbac_media';
+$conf['oscaddie_gcs_extensions'] = 'all';
+$conf['oscaddie_gcs_client_id'] = '363452670531-qfh0aklr59p6h86jcq0hkgfq6fpjd7ot.apps.googleusercontent.com';
+$conf['oscaddie_gcs_service_account_name'] = '363452670531-qfh0aklr59p6h86jcq0hkgfq6fpjd7ot@developer.gserviceaccount.com';
+$conf['oscaddie_gcs_signed_url_expiry'] = 300;
+$conf['oscaddie_gcs_library_version'] = 2;
 
 /**
  * Fast 404 settings:
