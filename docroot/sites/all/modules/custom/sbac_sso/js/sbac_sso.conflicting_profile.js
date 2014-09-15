c35d2663a1461cbb3beb49289833addda406ebe4
teknic <btate340@gmail.com>
2014-09-15 14:00:54 -0700

    updated css and js to match new placement of form

--- docroot/sites/all/modules/custom/sbac_sso/js/sbac_sso.conflicting_profile.js
+++ docroot/sites/all/modules/custom/sbac_sso/js/sbac_sso.conflicting_profile.js
@@ -1,14 +1,14 @@
 (function ($) {
   Drupal.behaviors = Drupal.behaviors || {};
 
-  Drupal.behaviors.sbac_user_conflicting_profile = {
+  Drupal.behaviors.sbac_sso_conflicting_profile = {
     attach: function (context, settings) {
       // Set OK button to disabled, user must select something.
-      $('.sbac-user-conflict-submit').attr('disabled', 'disabled');
-      $('.sbac-user-conflict-choice input').once('conflictingChoice', function() {
+      $('.sbac-sso-conflict-submit').attr('disabled', 'disabled');
+      $('.sbac-sso-conflict-choice input').once('conflictingChoice', function() {
         $(this).click( function() {
           // On click of a radio button, enable the submit.
-          $('.sbac-user-conflict-submit').removeAttr('disabled');
+          $('.sbac-sso-conflict-submit').removeAttr('disabled');
         });
       });
     }
