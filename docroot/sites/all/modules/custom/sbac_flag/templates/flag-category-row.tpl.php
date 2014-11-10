d3b72948ce88b6e42919629005067aac46bd0cca
nazli bozoglu <nzlbozoglu@gmail.com>
2014-11-10 17:31:24 -0500

    adds state and email of the user who flagged the item

--- docroot/sites/all/modules/custom/sbac_flag/templates/flag-category-row.tpl.php
+++ docroot/sites/all/modules/custom/sbac_flag/templates/flag-category-row.tpl.php
@@ -1,4 +1,12 @@
 <div class="flag-row">
   <div class="flag-user">By <?php echo $username; ?> on <?php echo $created; ?></div>
+    <div class="flag-email-state">
+	  <?php if (isset($user_email)): ?>
+	  	<a href= "mailto:<?php echo $user_email; ?>"> <?php echo $user_email; ?></a>
+	  <?php endif; ?>
+	  <?php if (isset($user_state)): ?>
+	  	, <?php echo $user_state; ?>
+	  <?php endif; ?>
+  </div>
   <div class="flag-message"><?php echo $flag_comment; ?></div>
 </div>
