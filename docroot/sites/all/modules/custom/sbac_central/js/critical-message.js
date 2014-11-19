9130b781f4ead13296a99972ed09bed627657272
nazli bozoglu <nzlbozoglu@gmail.com>
2014-11-19 00:32:02 -0500

    adding js function to close the popup when x is clicked

---
+++ docroot/sites/all/modules/custom/sbac_central/js/critical-message.js
@@ -0,0 +1,17 @@
+(function($) {
+    Drupal.behaviors.criticalMessageBehaviour = {};
+    Drupal.behaviors.criticalMessageBehaviour.attach = function() {
+        var showMessage = Drupal.settings.sbac_central.showCriticalMessage;
+        var message = Drupal.settings.sbac_central.message;
+        if (true === showMessage) {
+            Drupal.CTools.Modal.show();
+            $('#modal-content').html(message);
+            $(".cr-msg").click(function() {
+                $.get('/sbac-central-shown-message', function() {
+                    Drupal.CTools.Modal.dismiss();
+                });
+                return false;
+            });
+        }
+    };
+}) (jQuery);
