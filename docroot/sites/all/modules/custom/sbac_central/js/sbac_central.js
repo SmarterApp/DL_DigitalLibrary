9d11caf0034bbc18d7f2672c8c3f097c52dd2036
teknic <btate340@gmail.com>
2014-11-21 08:31:04 -0800

    added comments

--- docroot/sites/all/modules/custom/sbac_central/js/sbac_central.js
+++ docroot/sites/all/modules/custom/sbac_central/js/sbac_central.js
@@ -4,6 +4,7 @@
   Drupal.behaviors.sbac_central_critical_message = {
     attach: function (context, settings) {
       $(window).load( function() {
+        // Click the button for the critical message modal.
         if ($('#sbac-central-critical-message').length) {
           $('#sbac-central-critical-message').click();
           return false;
