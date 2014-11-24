29fd817c815b6dd4b2a34622bd8afc5d7a6743a4
teknic <btate340@gmail.com>
2014-11-24 14:59:12 -0800

    updated logic to fire critical message first, then welcome tutorial

--- docroot/sites/all/modules/custom/sbac_central/js/sbac_central.js
+++ docroot/sites/all/modules/custom/sbac_central/js/sbac_central.js
@@ -4,6 +4,18 @@
   Drupal.behaviors.sbac_central_critical_message = {
     attach: function (context, settings) {
       $(window).load( function() {
+        // If the user clicked esc key, check if we need to fire the
+        // Welcome Tutorial window by looking for a hidden element.
+        $(document).keyup(function(e) {
+          if (e.keyCode == 27) {
+            if ($('#sbac-central-click-welcome-tutorial').length) {
+              $('#sbac-help-welcome-tutorial').click();
+              $('#sbac-central-click-welcome-tutorial').remove();
+              return false;
+            }
+          }
+        });
+
         // Click the button for the critical message modal.
         if ($('#sbac-central-critical-message').length) {
           $('#sbac-central-critical-message').click();
