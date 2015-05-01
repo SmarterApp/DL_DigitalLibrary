f34c7c2fecbb6f69d172432772cacedfc277892b
aravenrose <raven_web@749006.no-reply.drupal.org>
2015-05-01 14:35:50 -0600

    Updated wording to be more clear on removing help topic from landing page, not everywhere, added a button to add help topics, fixed short theming, added a help edit button

--- docroot/sites/all/modules/custom/sbac_landing_page/templates/help-topics.tpl.php
+++ docroot/sites/all/modules/custom/sbac_landing_page/templates/help-topics.tpl.php
@@ -1,7 +1,8 @@
-<?php
-  if($topics):
-?>
+<?php if($topics): ?>
   <div id="show-hide-help-topics"></div>
+  <?php if($edit_help):?>
+  <div id="edit-help-topics"><?php print $edit_help; ?></div>
+  <?php endif; ?>
   <div class="help-topics-header"><strong>Need help?</strong> These resources will help you.</div>  
 <?php
 print $topics;
