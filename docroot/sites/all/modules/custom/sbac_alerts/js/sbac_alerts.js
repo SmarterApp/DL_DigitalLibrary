2b80c86ecb135711f078edcbd66c7e8b5d98a186
aravenrose <raven_web@749006.no-reply.drupal.org>
2015-05-15 13:10:54 -0600

    New module to display important alerts on the landing page

---
+++ docroot/sites/all/modules/custom/sbac_alerts/js/sbac_alerts.js
@@ -0,0 +1,44 @@
+(function ($) {
+  Drupal.behaviors.sbac_alerts = {
+    attach: function (context, settings) {
+      var alert_settings = Drupal.settings.sbac_alert_settings;
+      if(alert_settings){
+        if(getCookie('user-token') != alert_settings.token){
+          setCookie('user-token', '');
+          setCookie('display-notifications', '');
+        }        
+        if(getCookie('display-notifications') != 'hidden'){
+          $("#lp-notifications-wrapper").show(); 
+        }
+        $("#close-notifications").click(function () {
+          setCookie('user-token', alert_settings.token);
+          setCookie('display-notifications', 'hidden');
+          $("#lp-notifications-wrapper").fadeOut("slow");
+        });
+      }
+    }
+  };
+
+  // Set and expire cookie after 1 week
+  function setCookie(key, value) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + 7*24*60*60*1000);
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+  }
+
+  // Get cookie for testing
+  function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+  }
+  // Helper function to give a list of cookies
+  function listCookies() {
+    var theCookies = document.cookie.split(';');
+    var aString = '';
+    for (var i = 1; i <= theCookies.length; i++) {
+      aString += i + ' ' + theCookies[i - 1] + "\n";
+    }
+    return aString;
+  }
+  console.log(listCookies());
+})(jQuery);
--- docroot/sites/all/modules/custom/sbac_landing_page/templates/landing-page.tpl.php
+++ docroot/sites/all/modules/custom/sbac_landing_page/templates/landing-page.tpl.php
@@ -3,28 +3,34 @@
  * Template for SBAC landing page
  */
 ?>
-<?php if($help_topics): ?>
-<div id="lp-help-topics-wrapper">
-  <div id="lp-help-topics" class="row">
-    <?php print $help_topics; ?>
+<?php if($notifications): ?>
+  <div id="lp-notifications-wrapper">
+    <?php print $notifications; ?>
   </div>
-</div>
+<?php endif; ?>
+
+<?php if($help_topics): ?>
+  <div id="lp-help-topics-wrapper">
+    <div id="lp-help-topics" class="row">
+      <?php print $help_topics; ?>
+    </div>
+  </div>
 <?php endif; ?>
 
 <?php if($featured_content): ?>
-<div id="lp-featured-content-wrapper">
-  <div id="lp-featured-content">
-    <?php print $featured_content; ?>
+  <div id="lp-featured-content-wrapper">
+    <div id="lp-featured-content">
+      <?php print $featured_content; ?>
+    </div>
   </div>
-</div>
 <?php endif; ?>
 <?php if($find_resource): ?>
-<div id="lp-find-resource-wrapper">
-  <div id="lp-find-resource">
-    <div id="lp-find-resource-lable">What resources are you looking for?</div>
-    <?php print $find_resource; ?>
+  <div id="lp-find-resource-wrapper">
+    <div id="lp-find-resource">
+      <div id="lp-find-resource-lable">What resources are you looking for?</div>
+      <?php print $find_resource; ?>
+    </div>
   </div>
-</div>
 <?php endif; ?>
 
 <?php
