c6cbd9d24f77f9eb8860cee05aabd256c1d82e85
aravenrose <raven_web@749006.no-reply.drupal.org>
2015-05-20 11:13:04 -0600

    Removed featured resource wrapping anchor as nesting anchors in a tpl does not work. Added js to mimic same functionality

--- docroot/sites/all/modules/custom/sbac_featured_content/templates/sbac-featured_resource.tpl.php
+++ docroot/sites/all/modules/custom/sbac_featured_content/templates/sbac-featured_resource.tpl.php
@@ -7,47 +7,43 @@
 ?>
 
 <div class="slide featured-content-wrapper">
-  <div class="featured-content-inner">
-  <a href="<?php print $link;?>" class="featured-content-url featured-resource-url">
-  <div class="col-resource">
-  
-        <?php if($thumbnail):?>
-          <div class="featured-resource-image" style="background-image: url(<?php print $thumbnail; ?>)">
-          </div>
-        <?php endif; ?>
-          <p class="featured-label featured-type">Featured Resource</p>
-          <?php if($title):?>
-            <div class="featured-resource-title featured-title">
-              <?php print $title; ?>
-            </div>
-            <?php if($mini_profile):?>
-            <div class="featured-resource-mini-profile featured-mini-profile">
-              <?php print $mini_profile; ?>
-            </div>
-
-<?php endif; ?>
-
-      </div>
-  <div class="col-details">
-          <?php if($summary):?>
-            <div class="featured-resource-summary featured-summary">
-              <?php print $summary; ?>
-            </div>
-          <?php endif; ?>
-          <?php if($grades):?>
-            <div class="featured-content-terms-wrapper featured-resource-grades featured-grades">
-              <p class="featured-label">Grades</p>
-              <?php print $grades; ?>
-            </div>
-          <?php endif; ?>
-          <?php if($formative_process):?>
-            <div class="featured-content-terms-wrapper featured-resource-formative featured-formative">
-              <p class="featured-label">Formative Attributes</p>
-              <?php print $formative_process; ?>
-            </div>
-<?php endif; ?>
-  </div>
-  </a>
-<?php endif; ?>
+  <div class="featured-content-inner" url="<?php print $path ?>">
+    <div class="col-resource">
+      <?php if($thumbnail):?>
+        <a href="/<?php print $path ?>">
+          <div class="featured-resource-image" style="background-image: url(<?php print $thumbnail; ?>)"></div>
+        </a>
+      <?php endif; ?>
+      <p class="featured-label featured-type">Featured Resource</p>
+      <?php if($title):?>
+        <div class="featured-resource-title featured-title">
+          <a href="/<?php print $path ?>"><?php print $title; ?></a>
+        </div>
+      <?php endif;?>
+      <?php if($mini_profile):?>
+        <div class="featured-resource-mini-profile featured-mini-profile">
+          <?php print $mini_profile; ?>
+        </div>
+      <?php endif; ?>
+    </div>
+    <div class="col-details">
+      <?php if($summary):?>
+        <div class="featured-resource-summary featured-summary">
+          <?php print $summary; ?>
+        </div>
+      <?php endif; ?>
+      <?php if($grades):?>
+        <div class="featured-content-terms-wrapper featured-resource-grades featured-grades">
+          <p class="featured-label">Grades</p>
+          <?php print $grades; ?>
+        </div>
+      <?php endif; ?>
+      <?php if($formative_process):?>
+        <div class="featured-content-terms-wrapper featured-resource-formative featured-formative">
+          <p class="featured-label">Formative Attributes</p>
+          <?php print $formative_process; ?>
+        </div>
+      <?php endif; ?>
+    </div>
   </div>
 </div>
