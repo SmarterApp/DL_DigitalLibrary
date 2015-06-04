1ce1e13a91fa21426e031f3961d4d57418f17c63
Ethan Teague <ethan.teague@civicactions.com>
2015-06-03 20:44:53 -0400

    Adding template change and minor padding mod to change ordering on landing page.

--- docroot/sites/all/modules/custom/sbac_landing_page/templates/landing-page.tpl.php
+++ docroot/sites/all/modules/custom/sbac_landing_page/templates/landing-page.tpl.php
@@ -8,22 +8,6 @@
     <?php print $notifications; ?>
   </div>
 <?php endif; ?>
-
-<?php if($help_topics): ?>
-  <div id="lp-help-topics-wrapper">
-    <div id="lp-help-topics" class="row">
-      <?php print $help_topics; ?>
-    </div>
-  </div>
-<?php endif; ?>
-
-<?php if($featured_content): ?>
-  <div id="lp-featured-content-wrapper">
-    <div id="lp-featured-content">
-      <?php print $featured_content; ?>
-    </div>
-  </div>
-<?php endif; ?>
 <?php if($find_resource): ?>
   <div id="lp-find-resource-wrapper">
     <div id="lp-find-resource">
@@ -32,5 +16,17 @@
     </div>
   </div>
 <?php endif; ?>
-
-<?php
+<?php if($featured_content): ?>
+  <div id="lp-featured-content-wrapper">
+    <div id="lp-featured-content">
+      <?php print $featured_content; ?>
+    </div>
+  </div>
+<?php endif; ?>
+<?php if($help_topics): ?>
+  <div id="lp-help-topics-wrapper">
+    <div id="lp-help-topics" class="row">
+      <?php print $help_topics; ?>
+    </div>
+  </div>
+<?php endif; ?>
