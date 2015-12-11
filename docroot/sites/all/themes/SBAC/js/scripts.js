72eec246270dba179e72e049f15bea8730a56ca4
Ethan Teague <ethan.teague@civicactions.com>
2015-12-11 16:53:16 -0500

    Adding logic to close facets blocks when user clicks outside of filters div.

--- docroot/sites/all/themes/SBAC/js/scripts.js
+++ docroot/sites/all/themes/SBAC/js/scripts.js
@@ -3,17 +3,17 @@
 
   Drupal.behaviors.helpTextify = {
     attach: function (context, settings) {
-//      function initTextify(){
-//        $('#current-help-topic article .body').textify({
-//          numberOfColumn: 2,
-//          margin: 20,
-//          padding: 32,
-//          width: "auto",
-//          height: "400",
-//          showArrows : true,
-//          showNavigation: true
-//        });
-//      }
+      //      function initTextify(){
+      //        $('#current-help-topic article .body').textify({
+      //          numberOfColumn: 2,
+      //          margin: 20,
+      //          padding: 32,
+      //          width: "auto",
+      //          height: "400",
+      //          showArrows : true,
+      //          showNavigation: true
+      //        });
+      //      }
     }
   };
 
@@ -40,6 +40,7 @@
   Drupal.behaviors.toggleGrid = {
     attach: function (context, settings) {
       $(document).ready(function() {
+	// Controls the search box and plugs in a clear all x.
 	$('.form-item-sbac-digital-library-resources-text').append('<div id="search-x-clear"></div>');
 	if ($('#edit-sbac-digital-library-resources-text').val().length) {	  
 	  $('#search-x-clear').text('x');
@@ -49,16 +50,15 @@
 	})
 	
 	$('#edit-sbac-digital-library-resources-text').on('keyup', function() {
-	  if ($('#edit-sbac-digital-library-resources-text').val().length) {
-	    
+	  if ($('#edit-sbac-digital-library-resources-text').val().length) {	    
 	    $('#search-x-clear').text('x');
-
 	  }
 	  else {
 	    $('#search-x-clear').empty();
 	  }
 	})
-	
+
+	// Toggle between distinguished and all.
 	$('#faux-click-sbac').click(function(){
 	  if (window.location.href.indexOf('distinguished') > -1) {	    
 	    location.href = '/digital-library-resources';
@@ -72,6 +72,7 @@
 	  $('.view-digital-library-resources .views-row, #click-toggle-a').addClass('list');
 	}	
 
+	// Toggle cards list and grid view.
 	$('#click-toggle-a').click(function(e){
 	  $(this).toggleClass('list');
 	  $('.view-digital-library-resources .views-row').toggleClass('list');
@@ -85,18 +86,16 @@
 	  }
 	});
 
-	$('.collapsed, .expanded').each(function(){
-	  
+	// This is where we create the plus and minus signs for item list expansion.
+	$('.collapsed, .expanded').each(function() {	  
 	  if($(this).children('.facetapi-checkbox').hasClass('facetapi-inactive')){
 	    $(this).removeClass('minus-sign');
-	    $(this).addClass('plus-sign');
-	    
+	    $(this).addClass('plus-sign');	    
 	  }
 	  else{
 	    $(this).removeClass('plus-sign');
 	    $(this).addClass('minus-sign');
-	  }
-	  
+	  }	  
 	})
 
 	  $('.expanded').each(function(){
@@ -107,37 +106,36 @@
 
 	      if($(this).hasClass('plus-sign')){
 		$(this).addClass('minus-sign');
-		$(this).removeClass('plus-sign');
-		
+		$(this).removeClass('plus-sign');		
 	      }
 	      else{
 		$(this).addClass('plus-sign');
 		$(this).removeClass('minus-sign');		
-	      }
-	      
+	      }	      
 	    })
 	  })	  
-	    
-	  $('a.category-hide.filters-master').click(function() {
-	    if ($('.filters .inner-wrap').hasClass('not-activated-filters')) {	    
-	      $('.filters .inner-wrap').removeClass('not-activated-filters');
-	      $('.filters .inner-wrap').addClass('activated-filters');
-	      $('.filters .inner-wrap').animate({
-	      'height': '45px'
-	    });
-	  }
-	  else {
-	    $('.filters .inner-wrap').removeClass('activated-filters');
-	    $('.filters .inner-wrap').addClass('not-activated-filters');
-	    $('.filters .inner-wrap').animate({
-	      'height': '100%'
-	    }).css({
-	      'overflow': 'visible'
-	    });
-	  }	  
-	})
 
-	
+	    // Collapses the div that shows the facets.
+	    $('a.category-hide.filters-master').click(function() {
+	      if ($('.filters .inner-wrap').hasClass('not-activated-filters')) {	    
+		$('.filters .inner-wrap').removeClass('not-activated-filters');
+		$('.filters .inner-wrap').addClass('activated-filters');
+		$('.filters .inner-wrap').animate({
+		  'height': '45px'
+		});
+	      }
+	      else {
+		$('.filters .inner-wrap').removeClass('activated-filters');
+		$('.filters .inner-wrap').addClass('not-activated-filters');
+		$('.filters .inner-wrap').animate({
+		  'height': '100%'
+		}).css({
+		  'overflow': 'visible'
+		});
+	      }	  
+	    })
+
+	// Tracks the last active facet block to display open on page load.
 	$('#' + sessionStorage.getItem('lastClicked') + ' .facet-label a.category-hide').addClass('activated');
 	$('#' + sessionStorage.getItem('lastClicked') + ' .item-list').css({
 	  'height': '300px',
@@ -146,50 +144,56 @@
 	$('a.facetapi-checkbox').each(function(){
 	  $(this).text().replace('/(-)/', '');
 	})
-	
+	  
 	  $('.facetapi-checkbox').each(function(){
 	    $(this).click(function(){
 	      var theId = $(this).closest('.facet-block').attr('id');
 	      sessionStorage.setItem('lastClicked', theId)
 	    })
 	  })	
-	    
-	$('a.category-hide').each(function() {
-	  var targetList = $(this).parents().siblings().children().children().children(':checkbox:checked').length;	  
-	  if (targetList > 0) {
-	    $(this).addClass('selected');
-	    
-	  }
-	  else {
-	    $(this).removeClass('selected');
-	  }
-	  
-	  $(this).click(function() {
-	    $('*').not(this).removeClass('activated');
-	    $('.item-list').addClass('not-activated').css({
-	      'height': '0',
-	    });
-	    if ($(this).hasClass('activated')) {
-	      $(this).removeClass('activated').addClass('not-activated');	   
-	      $(this).parents().siblings('.item-list').css({
-		'height': '0',
-	      });
-	      
-	    }
-	    else {
 
-	      $(this).addClass('activated');
-	      $(this).removeClass('not-activated');
+	    // When user clicks outside of facets, close facet blocks.
+	    $('#main, .top-bar').click(function(){
+	      $('*').removeClass('activated');
 	      
-	      $(this).parents().siblings('.item-list').css({
-		'height': '300px',
+	      $('.item-list').css({
+		  'height': '0',
+		});
+	    })
+	    
+	    $('a.category-hide').each(function() {
+	      
+	      // Tracks active filter selections, changes styles accordingly.
+	      var targetList = $(this).parents().siblings().children().children().children(':checkbox:checked').length;	  
+	      if (targetList > 0) {
+		$(this).addClass('selected');		
+	      }
+	      else {
+		$(this).removeClass('selected');
+	      }
+	      
+	      $(this).click(function() {
+		$('*').not(this).removeClass('activated');
+		$('.item-list').addClass('not-activated').css({
+		  'height': '0',
+		});
+		if ($(this).hasClass('activated')) {
+		  $(this).removeClass('activated').addClass('not-activated');	   
+		  $(this).parents().siblings('.item-list').css({
+		    'height': '0',
+		  });		  
+		}
+		else {
+		  $(this).addClass('activated');
+		  $(this).removeClass('not-activated');		  
+		  $(this).parents().siblings('.item-list').css({
+		    'height': '300px',
+		  });
+		}
 	      });
-	    }
-	  })	  
-	})
-	
+	      
+	    });	
       });
-      
     }
   };
 
