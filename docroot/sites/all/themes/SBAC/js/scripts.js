1167e5851908a065b81dcc9447ae8b66fc0c11f0
Ethan Teague <ethan.teague@civicactions.com>
2015-12-11 21:18:33 -0500

    Fixing the toggle bug with an event.stopPropagation().

--- docroot/sites/all/themes/SBAC/js/scripts.js
+++ docroot/sites/all/themes/SBAC/js/scripts.js
@@ -1,22 +1,6 @@
 (function ($) {
   Drupal.behaviors = Drupal.behaviors || {};
 
-  Drupal.behaviors.helpTextify = {
-    attach: function (context, settings) {
-      //      function initTextify(){
-      //        $('#current-help-topic article .body').textify({
-      //          numberOfColumn: 2,
-      //          margin: 20,
-      //          padding: 32,
-      //          width: "auto",
-      //          height: "400",
-      //          showArrows : true,
-      //          showNavigation: true
-      //        });
-      //      }
-    }
-  };
-
   Drupal.behaviors.accountDropdown = {
     attach: function (context, settings) {
       $(document).click(function() {
@@ -87,7 +71,7 @@
 	});
 
 	// This is where we create the plus and minus signs for item list expansion.
-	$('.collapsed, .expanded').each(function() {	  
+	$('.expanded, .collapsed').each(function() {	  
 	  if($(this).children('.facetapi-checkbox').hasClass('facetapi-inactive')){
 	    $(this).removeClass('minus-sign');
 	    $(this).addClass('plus-sign');	    
@@ -98,45 +82,26 @@
 	  }	  
 	})
 
-	  $('.expanded').each(function(){
+	  $('.expanded, .collapsed').each(function(){
 	    $(this).addClass('plus-sign');
 	    $(this).removeClass('minus-sign');
-	    $(this).click(function(){
+	    $(this).click(function(event){
 	      $(this).children('.item-list').toggleClass('item-list-size');
 
 	      if($(this).hasClass('plus-sign')){
-		$(this).addClass('minus-sign');
-		$(this).removeClass('plus-sign');		
+		$(this).not($(this).parents().parents()).addClass('minus-sign');
+		$(this).not($(this).parents().parents()).removeClass('plus-sign');		
 	      }
 	      else{
-		$(this).addClass('plus-sign');
-		$(this).removeClass('minus-sign');		
-	      }	      
+		$(this).not($(this).parents().parents()).addClass('plus-sign');
+		$(this).not($(this).parents().parents()).removeClass('minus-sign');		
+	      }
+	      event.stopPropagation();
 	    })
 	  })	  
 
-	    // Collapses the div that shows the facets.
-	    $('a.category-hide.filters-master').click(function() {
-	      if ($('.filters .inner-wrap').hasClass('not-activated-filters')) {	    
-		$('.filters .inner-wrap').removeClass('not-activated-filters');
-		$('.filters .inner-wrap').addClass('activated-filters');
-		$('.filters .inner-wrap').animate({
-		  'height': '45px'
-		});
-	      }
-	      else {
-		$('.filters .inner-wrap').removeClass('activated-filters');
-		$('.filters .inner-wrap').addClass('not-activated-filters');
-		$('.filters .inner-wrap').animate({
-		  'height': '100%'
-		}).css({
-		  'overflow': 'visible'
-		});
-	      }	  
-	    })
-
-	// Tracks the last active facet block to display open on page load.
-	$('#' + sessionStorage.getItem('lastClicked') + ' .facet-label a.category-hide').addClass('activated');
+	    // Tracks the last active facet block to display open on page load.
+	    $('#' + sessionStorage.getItem('lastClicked') + ' .facet-label a.category-hide').addClass('activated');
 	$('#' + sessionStorage.getItem('lastClicked') + ' .item-list').css({
 	  'height': '300px',
 	});
@@ -157,54 +122,44 @@
 	      $('*').removeClass('activated');
 	      
 	      $('.item-list').css({
-		  'height': '0',
-		});
-	    })
-	    
-	    $('a.category-hide').each(function() {
-	      
-	      // Tracks active filter selections, changes styles accordingly.
-	      var targetList = $(this).parents().siblings().children().children().children(':checkbox:checked').length;	  
-	      if (targetList > 0) {
-		$(this).addClass('selected');		
-	      }
-	      else {
-		$(this).removeClass('selected');
-	      }
-	      
-	      $(this).click(function() {
-		$('*').not(this).removeClass('activated');
-		$('.item-list').addClass('not-activated').css({
-		  'height': '0',
-		});
-		if ($(this).hasClass('activated')) {
-		  $(this).removeClass('activated').addClass('not-activated');	   
-		  $(this).parents().siblings('.item-list').css({
-		    'height': '0',
-		  });		  
-		}
-		else {
-		  $(this).addClass('activated');
-		  $(this).removeClass('not-activated');		  
-		  $(this).parents().siblings('.item-list').css({
-		    'height': 'auto',
-		    'max-height': '300px'
-		  });
-		}
+		'height': '0',
 	      });
-	      
-	    });	
+	    })
+	
+	$('a.category-hide').each(function() {
+	  
+	  // Tracks active filter selections, changes styles accordingly.
+	  var targetList = $(this).parents().siblings().children().children().children(':checkbox:checked').length;	  
+	  if (targetList > 0) {
+	    $(this).addClass('selected');		
+	  }
+	  else {
+	    $(this).removeClass('selected');
+	  }
+	  
+	  $(this).click(function() {
+	    $('*').not(this).removeClass('activated');
+	    $('.item-list').addClass('not-activated').css({
+	      'height': '0',
+	    });
+	    if ($(this).hasClass('activated')) {
+	      $(this).removeClass('activated').addClass('not-activated');	   
+	      $(this).parents().siblings('.item-list').css({
+		'height': '0',
+	      });		  
+	    }
+	    else {
+	      $(this).addClass('activated');
+	      $(this).removeClass('not-activated');		  
+	      $(this).parents().siblings('.item-list').css({
+		'height': 'auto',
+		'max-height': '300px'
+	      });
+	    }
+	  });
+	  
+	});	
       });
-    }
-  };
-
-  Drupal.behaviors.toggleFilters = {
-    attach: function (context, settings) {
-      $(document).ready(function(){
-	$('a.category-hide').click(function() {
-
-	});
-      })
     }
   };
   
