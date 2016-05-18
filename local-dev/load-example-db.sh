37f78e40f6a7db51b79a76d023a11d497183e135
David Ward <dward@wevad.com>
2016-05-18 13:22:36 -0700

    update initial setup-local script

--- local-dev/load-example-db.sh
+++ local-dev/load-example-db.sh
@@ -8,6 +8,7 @@ if [ ! -f cache/sbac.sql.gz ]; then
   docker run --rm -v "$(pwd)/cache:/dls:rw" appropriate/curl -fsSL -o /dls/sbac.sql.gz https://github.com/SmarterApp/DigitalLibrary/raw/59ff35278b6c82273105af88f340079434f522a5/docroot/profiles/sbac/sbac.sql.gz
 fi
 
+./exec-drush sql-drop -y
 gzip -dc cache/sbac.sql.gz \
   | docker exec -i ${COMPOSE_PROJECT_NAME}_db_1 sh -c 'exec mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" cms'
 
--- local-dev/setup-local.sh
+++ local-dev/setup-local.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
 cd $(dirname $0)
+
+echo 'Bring up containers'
+docker-compose up -d
+
+
 cd ..
 
+echo 'Install code via composer'
+PWD=$(pwd)
+DIR=/var/www/html
+docker run --rm -v ${PWD}:${DIR} -w ${DIR} geshan/php-composer-alpine "composer install"
+
+echo 'Apply patches'
 # Patch for issue where get_resources() is redefined
 patch -p1 < local-dev/patches/001-sbac_report.patch
 
 
 cd $(dirname $0)
 
+echo 'Load Example DB'
+./load-example-db.sh
+
+echo 'Adjust settings via drush'
 ./exec-drush dis google_analytics sbac_sso acquia_spi learning_registry
 ./exec-drush user-password admin --password="password"
 ./exec-drush vset reroute_email_address beads2nibs@mailinator.com
-./exec-drush vset reroute_enable TRUE
+./exec-drush vset reroute_enable 1
 
 ./exec-drush cc all
