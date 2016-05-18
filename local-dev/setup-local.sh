39bc088163515b3b3067b9a5538d7b51a08e1af7
David Ward <dward@wevad.com>
2016-05-18 16:42:43 -0700

    update scripts to set up local environment

--- local-dev/setup-local.sh
+++ local-dev/setup-local.sh
@@ -2,23 +2,29 @@
 
 cd $(dirname $0)
 
-echo 'Bring up containers'
-docker-compose up -d
-
+. ./.env
 
 cd ..
 
 echo 'Install code via composer'
 PWD=$(pwd)
 DIR=/var/www/html
-docker run --rm -v ${PWD}:${DIR} -w ${DIR} geshan/php-composer-alpine "composer install"
+docker run --rm -v ${PWD}:${DIR} -w ${DIR} composer/composer install
 
 echo 'Apply patches'
 # Patch for issue where get_resources() is redefined
 patch -p1 < local-dev/patches/001-sbac_report.patch
-
+cp local-dev/templates/settings.php docroot/sites/default/
+cp local-dev/templates/local_settings.inc docroot/sites/default/
+ln -s contrib/zurb_foundation docroot/sites/all/themes/zurb_foundation
+ln -s custom/SBAC docroot/sites/all/themes/SBAC
+mkdir -m 0777 docroot/sites/default/files
+mkdir -m 0777 docroot/sites/default/files-private
 
 cd $(dirname $0)
+
+echo 'Bring up containers'
+docker-compose up -d
 
 echo 'Load Example DB'
 ./load-example-db.sh
@@ -30,3 +36,10 @@ echo 'Adjust settings via drush'
 ./exec-drush vset reroute_enable 1
 
 ./exec-drush cc all
+
+
+echo
+echo
+echo 'Setup complete:'
+echo
+echo 'You can connect to http://' $(docker inspect ${COMPOSE_PROJECT_NAME}_nginx_1 --format "{{ .NetworkSettings.Networks.${COMPOSE_PROJECT_NAME}_default.IPAddress }}")
---
+++ local-dev/templates/settings.php
@@ -0,0 +1,33 @@
+<?php
+
+$update_free_access = FALSE;
+$drupal_hash_salt = '';
+
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 1);
+ini_set('session.gc_maxlifetime', 86400);
+ini_set('session.cookie_lifetime', 86400);
+
+$conf['404_fast_paths_exclude'] = '/\/(?:styles)\//';
+$conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
+$conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
+
+// Local Environment settings.
+$file = __DIR__ . '/local_settings.inc';
+if (file_exists($file)) {
+  include_once $file;
+}
+
+include_once('./sites/all/modules/contrib/fast_404/fast_404.inc');
+
+$conf['fast_404_exts'] = '/[^robots]\.(txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
+$conf['fast_404_allow_anon_imagecache'] = TRUE;
+
+//$conf['fast_404_exts'] = '/\.(txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp|php|html?|xml)$/i';
+
+$conf['fast_404_html'] = '<html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
+$conf['fast_404_path_check'] = FALSE;
+$conf['fast_404_url_whitelisting'] = FALSE;
+$conf['fast_404_whitelist'] = array('index.php', 'rss.xml', 'install.php', 'cron.php', 'update.php', 'xmlrpc.php');
+$conf['fast_404_string_whitelisting'] = array('cdn/farfuture', '/advagg_', 'html5_module', 'resources', 'resource_stats_csv', 'resource_thumbnails', 'html5_thumbnails');
+$conf['fast_404_HTML_error_all_paths'] = FALSE;
