0c5276a702f82a1187a4008e88e2e35df59bbfc9
David Ward <dward@wevad.com>
2016-05-25 07:23:57 -0700

    adjust the setup script to be re-runable

--- local-dev/setup-local.sh
+++ local-dev/setup-local.sh
@@ -13,13 +13,30 @@ docker run --rm -v ${PWD}:${DIR} -w ${DIR} composer/composer install
 
 echo 'Apply patches'
 # Patch for issue where get_resources() is redefined
-patch -p1 < local-dev/patches/001-sbac_report.patch
+if [ "$(grep -n sbac_report_get_resources docroot/sites/all/modules/custom/sbac_report/sbac_report.resource_stats.inc)" -eq 0 ]; then
+  patch -p1 < local-dev/patches/001-sbac_report.patch
+fi
+
+# Install settings files
 cp local-dev/templates/settings.php docroot/sites/default/
 cp local-dev/templates/local_settings.inc docroot/sites/default/
-ln -s contrib/zurb_foundation docroot/sites/all/themes/zurb_foundation
-ln -s custom/SBAC docroot/sites/all/themes/SBAC
-mkdir -m 0777 docroot/sites/default/files
-mkdir -m 0777 docroot/sites/default/files-private
+
+# Create symlinks as currently the themes need to be at .../themes/ instead of .../themes/{contrib,custome}/
+if [ ! -h docroot/sites/all/themes/zurb_foundation ]; then
+  ln -s contrib/zurb_foundation docroot/sites/all/themes/zurb_foundation
+fi
+if [ ! -h docroot/sites/all/themes/SBAC ]; then
+  ln -s custom/SBAC docroot/sites/all/themes/SBAC
+fi
+
+# Make sure the files directory exists
+if [ ! -d docroot/sites/default/files ]; then
+  mkdir -m 0777 docroot/sites/default/files
+fi
+if [ ! -d docroot/sites/default/files-private ]; then
+  mkdir -m 0777 docroot/sites/default/files-private
+fi
+
 
 cd $(dirname $0)
 
