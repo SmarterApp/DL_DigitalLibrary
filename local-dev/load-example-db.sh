149f76eabc023ebe66084953319931a8d0c07cf1
David Ward <dward@wevad.com>
2016-06-06 13:50:54 -0700

    use updated version of sbac seed database to work with entitycache v 7.1.5

--- local-dev/load-example-db.sh
+++ local-dev/load-example-db.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 . ./.env
 
 if [ ! -f cache/sbac.sql.gz ]; then
-  docker run --rm -v "$(pwd)/cache:/dls:rw" appropriate/curl -fsSL -o /dls/sbac.sql.gz https://github.com/SmarterApp/DigitalLibrary/raw/59ff35278b6c82273105af88f340079434f522a5/docroot/profiles/sbac/sbac.sql.gz
+  docker run --rm -v "$(pwd)/cache:/dls:rw" appropriate/curl -fsSL -o /dls/sbac.sql.gz https://s3.amazonaws.com/uploads.hipchat.com/519721/3548552/2KeuTLLk3OH2acM/local-db-20160606T1130.sql.gz
 fi
 
 ./exec-drush sql-drop -y
