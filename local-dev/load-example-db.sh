6988b839ca69d5144019c503be105ad6e222970f
David Ward <dward@wevad.com>
2016-06-07 11:49:28 -0700

    fetch sample database from bitbucket

--- local-dev/load-example-db.sh
+++ local-dev/load-example-db.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 . ./.env
 
 if [ ! -f cache/sbac.sql.gz ]; then
-  docker run --rm -v "$(pwd)/cache:/dls:rw" appropriate/curl -fsSL -o /dls/sbac.sql.gz https://s3.amazonaws.com/uploads.hipchat.com/519721/3548552/2KeuTLLk3OH2acM/local-db-20160606T1130.sql.gz
+  docker run --rm -v "$(pwd)/cache:/dls:rw" appropriate/curl -fsSL -o /dls/sbac.sql.gz https://bitbucket.org/pcgsbacdev/sample-db/raw/2178aee135b600746fc78ca3d9cc2375231505cd/sbac.sql.gz
   if [ ! -f cache/sbac.sql.gz ]; then
     echo 'Error downloading sql file'
     exit 1
@@ -15,4 +15,3 @@ fi
 ./exec-drush sql-drop -y
 gzip -dc cache/sbac.sql.gz \
   | docker exec -i ${COMPOSE_PROJECT_NAME}_db_1 sh -c 'exec mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" cms'
-
