d1d3592afff0b56557b0424951feccebf55417cc
Erik Levinson <eriklevinson@Eriks-MacBook-Pro.local>
2016-07-01 14:09:14 -0600

    Updated acquia hooks

---
+++ hooks/dev/post-code-update/drush-cache-clear.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Cloud Hook: drush-cache-clear
+#
+# Run drush cache-clear all in the target environment. This script works as
+# any Cloud hook.
+
+
+# Map the script inputs to convenient names.
+site=$1
+target_env=$2
+drush_alias=$site'.'$target_env
+
+# Execute a standard drush command.
+# drush @$drush_alias cc all
+# drush @$drush_alias fra -y
+# drush @$drush_alias updb -y
+# drush @$drush_alias cc all
+# drush @$drush_alias fra -y
