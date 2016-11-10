f79f680afec1189dcb9e8b093eea56b5742204c7
David Ward <dward@wevad.com>
2016-11-10 12:10:10 -0800

    enable reroute_email in acquia deploy/update hooks (not prod/stage2)

--- hooks/load/post-code-deploy/z-drush-cache-clear.sh
+++ hooks/load/post-code-deploy/z-drush-cache-clear.sh
@@ -18,5 +18,6 @@ drush_alias=$site'.'$target_env
  drush @$drush_alias cc all
  drush @$drush_alias fra -y
 
+ drush @$drush_alias en -y reroute_email
  drush @$drush_alias vset reroute_email_address dlmailtest@gedu-demo-pcg.com
  drush @$drush_alias vset reroute_enable 1
