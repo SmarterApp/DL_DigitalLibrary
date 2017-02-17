b12b36336d835e920a34ab08eaee2eca47deb2d0
Jim Baker <jbaker@pcgus.com>
2017-02-17 15:12:54 -0500

    SBAC-323: Only show the front page sign-up button to non-contributors.

--- docroot/sites/all/themes/custom/SBAC/templates/sbac_landing_page/landing-page.tpl.php
+++ docroot/sites/all/themes/custom/SBAC/templates/sbac_landing_page/landing-page.tpl.php
@@ -30,8 +30,10 @@
     </div>
     <div id="leaderboards">
       <?php print $leaderboards; ?>
+      <?php if (!in_array('resource reviewer', $user->roles) && !in_array('resource contributor', $user->roles) && !in_array('resource publisher', $user->roles)): ?>
         <div id="sne-signup-spacer"></div>
         <div id="sne-signup">I am interested in contributing resources to the Digital Library <?php print l('Submit', '/sne-signup', array('attributes' => array('class' => array('button', 'sne-link')))); ?></div>
+      <?php endif; ?>
     </div>
   </div>
 <?php endif; ?>
