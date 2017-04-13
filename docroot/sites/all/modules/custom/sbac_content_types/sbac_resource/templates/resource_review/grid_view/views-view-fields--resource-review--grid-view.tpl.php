3fa93613736851b7e3cf4b3068b37159909a5b61
Jim Baker <jbaker@pcgus.com>
2017-04-13 10:52:00 -0400

    Truncate the resource type since long ones will run into the Resubmission indicator.

--- docroot/sites/all/modules/custom/sbac_content_types/sbac_resource/templates/resource_review/grid_view/views-view-fields--resource-review--grid-view.tpl.php
+++ docroot/sites/all/modules/custom/sbac_content_types/sbac_resource/templates/resource_review/grid_view/views-view-fields--resource-review--grid-view.tpl.php
@@ -51,7 +51,7 @@
     <?php endif; ?>
     <div class='resource-type'>
       <?php if (isset($fields['field_focus'])): ?>
-        <?php echo $fields['field_focus']->content; ?>
+        <?php echo truncate_utf8(strip_tags($fields['field_focus']->content), 27, FALSE, TRUE); ?>
       <?php endif; ?>
     </div>
   </div>
